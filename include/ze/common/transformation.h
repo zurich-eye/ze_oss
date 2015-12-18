0972e2280fe8efb83c01eadfaa6e8e373982359d
Christian Forster <forster@ifi.uzh.ch>
2015-12-18 15:23:19 +0100

    started adding some useful tools.

---
+++ include/ze/common/transformation.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <kindr/minimal/quat-transformation.h>
+
+namespace ze {
+
+using Transformation = kindr::minimal::QuatTransformation;
+using Quaternion = kindr::minimal::RotationQuaternion;
+
+} // namespace ze
