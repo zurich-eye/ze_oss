f6feb09cd97fcc4326a2ea282feca3d5fd44a1a1
Christian Forster <forster@ifi.uzh.ch>
2016-01-06 13:47:21 +0100

    add alignment example file.

---
+++ examples/trajectory_alignment.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# This example shows how to:
+# 1) Load two trajectories
+# 2) Find closest timestamps
+# 3) Align the trajectories
+# 4) Compute error statistics
+
+import ze_py.test.utils as test_utils
+
+test_data_dir = test_utils.get_test_data_dir('ze_applanix_gt_data')
+
+# TODO
