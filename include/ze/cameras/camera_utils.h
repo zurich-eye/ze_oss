3ae2e0723235428bd1ab3528dc889d6f65d5a4f1
Christian Forster <forster@ifi.uzh.ch>
2016-01-10 13:03:18 +0100

    pose optimizer based on bearing vectors.

--- include/ze/cameras/camera_utils.h
+++ include/ze/cameras/camera_utils.h
@@ -4,16 +4,9 @@
 
 namespace ze {
 
-// Generate count random visible keypoints.
+//! Generate count random visible keypoints.
 Keypoints generateRandomKeypoints(
     const int image_width, const int image_height, const int margin,
     const size_t count);
-
-// Normalize a block of bearing vectors.
-inline void normalizeBearings(
-    Bearings& bearings)
-{
-  bearings = bearings.array().rowwise() / bearings.colwise().norm().array();
-}
 
 } // namespace ze
