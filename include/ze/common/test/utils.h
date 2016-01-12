851c8b6b453ea2931cd70320ec8c16f71ad9353a
Christian Forster <forster@ifi.uzh.ch>
2016-01-12 21:10:52 +0100

    working on patch-warp.

--- include/ze/common/test/utils.h
+++ include/ze/common/test/utils.h
@@ -12,7 +12,9 @@ std::string getTestDataDir(const std::string& dataset_name)
   const char* datapath_dir = std::getenv("ZE_TEST_DATA_PATH");
   CHECK_NOTNULL(datapath_dir);
   std::string path(datapath_dir);
-  CHECK(isDir(path)) << "Folder does not exist: " << path;
+  CHECK(isDir(path)) << "Folder does not exist: " << path << "."
+                     << "Did you download the ze_test_data repository and set "
+                     << "the ZE_TEST_DATA_PATH environment variable?";
   path = path + "/data/" + dataset_name;
   CHECK(isDir(path)) << "Dataset does not exist: " << path;
   return path;
