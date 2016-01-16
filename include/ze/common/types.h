3d61df7cb6d4e797ff71aea7c1615e04d66dc810
Christian Forster <forster@ifi.uzh.ch>
2016-01-16 12:53:18 +0100

    addressed comments.

--- include/ze/common/types.h
+++ include/ze/common/types.h
@@ -10,6 +10,7 @@ namespace ze {
 using size_t    = std::size_t;
 using int64_t   = std::int64_t;   
 using uint8_t   = std::uint8_t;
+using uint32_t  = std::uint32_t;
 using uint64_t  = std::uint64_t;
 using FloatType = double;
 
@@ -18,16 +19,16 @@ using FloatType = double;
 
 // MatrixMN, MatrixN = MatrixNN, I_NxN, and Z_NxN, for M,N=1..9.
 #define ZE_MAKE_EIGEN_MATRIX_TYPEDEFS(SIZE, SUFFIX)         \
-  using Matrix##SUFFIX = Eigen::Matrix<double, SIZE, SIZE>; \
-  using Matrix1##SUFFIX = Eigen::Matrix<double, 1, SIZE>;   \
-  using Matrix2##SUFFIX = Eigen::Matrix<double, 2, SIZE>;   \
-  using Matrix3##SUFFIX = Eigen::Matrix<double, 3, SIZE>;   \
-  using Matrix4##SUFFIX = Eigen::Matrix<double, 4, SIZE>;   \
-  using Matrix5##SUFFIX = Eigen::Matrix<double, 5, SIZE>;   \
-  using Matrix6##SUFFIX = Eigen::Matrix<double, 6, SIZE>;   \
-  using Matrix7##SUFFIX = Eigen::Matrix<double, 7, SIZE>;   \
-  using Matrix8##SUFFIX = Eigen::Matrix<double, 8, SIZE>;   \
-  using Matrix9##SUFFIX = Eigen::Matrix<double, 9, SIZE>;   \
+  using Matrix##SUFFIX = Eigen::Matrix<FloatType, SIZE, SIZE>; \
+  using Matrix1##SUFFIX = Eigen::Matrix<FloatType, 1, SIZE>;   \
+  using Matrix2##SUFFIX = Eigen::Matrix<FloatType, 2, SIZE>;   \
+  using Matrix3##SUFFIX = Eigen::Matrix<FloatType, 3, SIZE>;   \
+  using Matrix4##SUFFIX = Eigen::Matrix<FloatType, 4, SIZE>;   \
+  using Matrix5##SUFFIX = Eigen::Matrix<FloatType, 5, SIZE>;   \
+  using Matrix6##SUFFIX = Eigen::Matrix<FloatType, 6, SIZE>;   \
+  using Matrix7##SUFFIX = Eigen::Matrix<FloatType, 7, SIZE>;   \
+  using Matrix8##SUFFIX = Eigen::Matrix<FloatType, 8, SIZE>;   \
+  using Matrix9##SUFFIX = Eigen::Matrix<FloatType, 9, SIZE>;   \
   static const Eigen::MatrixBase<Matrix##SUFFIX>::IdentityReturnType I_##SUFFIX##x##SUFFIX = Matrix##SUFFIX::Identity(); \
   static const Eigen::MatrixBase<Matrix##SUFFIX>::ConstantReturnType Z_##SUFFIX##x##SUFFIX = Matrix##SUFFIX::Zero()
 
@@ -42,18 +43,19 @@ ZE_MAKE_EIGEN_MATRIX_TYPEDEFS(8,8);
 ZE_MAKE_EIGEN_MATRIX_TYPEDEFS(9,9);
 
 // Typedef arbitary length vector.
-typedef Eigen::VectorXd Vector;
+using Vector  = Eigen::Matrix<FloatType, Eigen::Dynamic, 1>;
 
 // Commonly used fixed size vectors.
-using Vector1 = Eigen::Matrix<double, 1, 1>;
-using Vector2 = Eigen::Vector2d;
-using Vector3 = Eigen::Vector3d;
-using Vector4 = Eigen::Matrix<double, 4, 1>;
-using Vector5 = Eigen::Matrix<double, 5, 1>;
-using Vector6 = Eigen::Matrix<double, 6, 1>;
-using Vector7 = Eigen::Matrix<double, 7, 1>;
-using Vector8 = Eigen::Matrix<double, 8, 1>;
-using Vector9 = Eigen::Matrix<double, 9, 1>;
+using Vector1 = Eigen::Matrix<FloatType, 1, 1>;
+using Vector2 = Eigen::Matrix<FloatType, 2, 1>;
+using Vector3 = Eigen::Matrix<FloatType, 3, 1>;
+using Vector4 = Eigen::Matrix<FloatType, 4, 1>;
+using Vector5 = Eigen::Matrix<FloatType, 5, 1>;
+using Vector6 = Eigen::Matrix<FloatType, 6, 1>;
+using Vector7 = Eigen::Matrix<FloatType, 7, 1>;
+using Vector8 = Eigen::Matrix<FloatType, 8, 1>;
+using Vector9 = Eigen::Matrix<FloatType, 9, 1>;
+using Vector2i = Eigen::Vector2i;
 
 //------------------------------------------------------------------------------
 // Feature containers.
