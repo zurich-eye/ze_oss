eb8efb218ac8a839a902a1cf97b7caf9a5bd1208
Christian Forster <forster@ifi.uzh.ch>
2016-01-20 21:47:16 +0100

    implement factory method for GpuPyramid - UNTESTED (there are no test cases using gpu pyramid??)

--- include/imp/cu_imgproc/image_pyramid.hpp
+++ include/imp/cu_imgproc/image_pyramid.hpp
@@ -7,15 +7,20 @@
 
 namespace ze {
 
-/*
-// Factory method
-template<typename Pixel, ze::PixelType pixel_type>
-ImagePyramid<Pixel,pixel_type>::Ptr createPyramid(
-    ImagePtr img_level0, InterpolationMode interp)
+//! Image Pyramid Factory:
+template<typename Pixel>
+std::shared_ptr<ImagePyramid<Pixel>>
+createImagePyramidGpu(
+    typename Image<Pixel, pixel_type<Pixel>::type>::Ptr img_level0,
+    FloatType scale_factor, uint32_t max_num_levels, uint32_t size_bound)
 {
-  // TODO sanity checks
+  // Sanity checks.
+  CHECK(img_level0->isGpuMemory());
+  using Pyr = ImagePyramid<Pixel>;
+  auto pyr = std::make_shared<Pyr>(
+        img_level0->size(), scale_factor, size_bound, max_num_levels);
 
-  levels_.push_back(img_level0);
+  pyr->push_back(img_level0);
   Size2u sz0 =  img_level0->size();
 
   for (size_t i=1; i<num_levels_; ++i)
@@ -24,25 +29,12 @@ ImagePyramid<Pixel,pixel_type>::Ptr createPyramid(
               static_cast<std::uint32_t>(sz0.height()*scale_factors_[i] + 0.5f));
 
     // init level memory with either ImageGpu or ImageRaw
-    if(img_level0->isGpuMemory())
-    {
-      using ImageGpu = typename ze::cu::ImageGpu<Pixel,pixel_type>;
-      typename ImageGpu::Ptr img = std::make_shared<ImageGpu>(sz);
-      typename ImageGpu::Ptr prev = std::dynamic_pointer_cast<ImageGpu>(levels_.back());
-      ze::cu::reduce(*img, *prev, interp, true);
-      levels_.push_back(img);
-    }
-    else
-    {
-      using ImageRaw = ze::ImageRaw<Pixel,pixel_type>;
-      typename ImageRaw::Ptr img = std::make_shared<ImageRaw>(sz);
-      //! @todo (MWE) cpu reduction
-      throw ze::Exception("CPU reduction not yet implemented.", __FILE__, __FUNCTION__, __LINE__);
-      levels_.push_back(img);
-    }
+    using ImageGpu = typename ze::cu::ImageGpu<Pixel,pixel_type>;
+    pyr->emplace_back(std::make_shared<ImageGpu>(sz));
+    typename ImageGpu::Ptr prev = std::dynamic_pointer_cast<ImageGpu>(levels_.back());
+    typename ImageGpu& img = pyr->at(i);
+    ze::cu::reduce(img, *prev, interp, true);
   }
 }
-*/
-
 
 } // namespace ze
