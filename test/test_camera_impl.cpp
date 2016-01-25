5dc27fd5a6563a0a06371ea9630d60bcce9d7082
Christian Forster <forster@ifi.uzh.ch>
2016-01-25 21:57:57 +0100

    fix after merge.

--- test/test_camera_impl.cpp
+++ test/test_camera_impl.cpp
@@ -3,8 +3,9 @@
 #include <iostream>
 #include <functional>
 
-#include <ze/common/test/entrypoint.h>
-#include <ze/common/test/utils.h>
+#include <ze/common/test_entrypoint.h>
+#include <ze/common/test_utils.h>
+#include <ze/common/path_utils.h>
 #include <ze/common/matrix.h>
 #include <ze/common/manifold.h>
 #include <ze/common/numerical_derivative.h>
