7cece383399dff68aaa969833d64112af04f82ea
Matia Pizzoli <matia.pizzoli@gmail.com>
2016-02-10 19:29:15 +0100

    added position series

--- test/test_ts_match.cpp
+++ test/test_ts_match.cpp
@@ -8,7 +8,7 @@
 #include <ze/common/time.h>
 #include <ze/common/test_utils.h>
 #include <ze/ts_match.h>
-
+#include <ze/common/csv_trajectory.h>
 #include <ze/common/test_entrypoint.h>
 
 // Matching parameters
