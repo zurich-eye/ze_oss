4365f1b30b0b8296c8ce4f29d9f1c3ef18f86754
Matia Pizzoli <matia.pizzoli@gmail.com>
2016-02-25 00:25:25 +0100

    updated series of leap seconds

--- include/ze/common/gps_time.h
+++ include/ze/common/gps_time.h
@@ -37,30 +37,35 @@ OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 
 #include <time.h>
+#include <stdint.h>
+
+namespace ze {
 
 constexpr double gpst0[]={1980,1, 6,0,0,0}; /* gps time reference */
 
 constexpr double leaps[][7]={ /* leap seconds {y,m,d,h,m,s,utc-gpst,...} */
-    {2009,1,1,0,0,0,-15},
-    {2006,1,1,0,0,0,-14},
-    {1999,1,1,0,0,0,-13},
-    {1997,7,1,0,0,0,-12},
-    {1996,1,1,0,0,0,-11},
-    {1994,7,1,0,0,0,-10},
-    {1993,7,1,0,0,0, -9},
-    {1992,7,1,0,0,0, -8},
-    {1991,1,1,0,0,0, -7},
-    {1990,1,1,0,0,0, -6},
-    {1988,1,1,0,0,0, -5},
-    {1985,7,1,0,0,0, -4},
-    {1983,7,1,0,0,0, -3},
-    {1982,7,1,0,0,0, -2},
-    {1981,7,1,0,0,0, -1}
-};
+                              {2015,1,1,0,0,0,-17},
+                              {2012,1,1,0,0,0,-16},
+                              {2009,1,1,0,0,0,-15},
+                              {2006,1,1,0,0,0,-14},
+                              {1999,1,1,0,0,0,-13},
+                              {1997,7,1,0,0,0,-12},
+                              {1996,1,1,0,0,0,-11},
+                              {1994,7,1,0,0,0,-10},
+                              {1993,7,1,0,0,0, -9},
+                              {1992,7,1,0,0,0, -8},
+                              {1991,1,1,0,0,0, -7},
+                              {1990,1,1,0,0,0, -6},
+                              {1988,1,1,0,0,0, -5},
+                              {1985,7,1,0,0,0, -4},
+                              {1983,7,1,0,0,0, -3},
+                              {1982,7,1,0,0,0, -2},
+                              {1981,7,1,0,0,0, -1}
+                            };
 
 typedef struct {        /* time struct */
-    time_t time;        /* time (s) expressed by standard time_t */
-    double sec;         /* fraction of second under 1 s */
+  time_t time;        /* time (s) expressed by standard time_t */
+  double sec;         /* fraction of second under 1 s */
 } gtime_t;
 
 /* convert calendar day/time to time -------------------------------------------
@@ -71,18 +76,18 @@ typedef struct {        /* time struct */
 *-----------------------------------------------------------------------------*/
 extern gtime_t epoch2time(const double *ep)
 {
-    const int doy[]={1,32,60,91,121,152,182,213,244,274,305,335};
-    gtime_t time={0};
-    int days,sec,year=(int)ep[0],mon=(int)ep[1],day=(int)ep[2];
+  const int doy[]={1,32,60,91,121,152,182,213,244,274,305,335};
+  gtime_t time={0};
+  int days,sec,year=(int)ep[0],mon=(int)ep[1],day=(int)ep[2];
 
-    if (year<1970||2099<year||mon<1||12<mon) return time;
+  if (year<1970||2099<year||mon<1||12<mon) return time;
 
-    /* leap year if year%4==0 in 1901-2099 */
-    days=(year-1970)*365+(year-1969)/4+doy[mon-1]+day-2+(year%4==0&&mon>=3?1:0);
-    sec=(int)floor(ep[5]);
-    time.time=(time_t)days*86400+(int)ep[3]*3600+(int)ep[4]*60+sec;
-    time.sec=ep[5]-sec;
-    return time;
+  /* leap year if year%4==0 in 1901-2099 */
+  days=(year-1970)*365+(year-1969)/4+doy[mon-1]+day-2+(year%4==0&&mon>=3?1:0);
+  sec=(int)floor(ep[5]);
+  time.time=(time_t)days*86400+(int)ep[3]*3600+(int)ep[4]*60+sec;
+  time.sec=ep[5]-sec;
+  return time;
 }
 
 /* add time --------------------------------------------------------------------
@@ -93,10 +98,10 @@ extern gtime_t epoch2time(const double *ep)
 *-----------------------------------------------------------------------------*/
 extern gtime_t timeadd(gtime_t t, double sec)
 {
-    double tt;
+  double tt;
 
-    t.sec+=sec; tt=floor(t.sec); t.time+=(int)tt; t.sec-=tt;
-    return t;
+  t.sec+=sec; tt=floor(t.sec); t.time+=(int)tt; t.sec-=tt;
+  return t;
 }
 /* time difference -------------------------------------------------------------
 * difference between gtime_t structs
@@ -105,7 +110,7 @@ extern gtime_t timeadd(gtime_t t, double sec)
 *-----------------------------------------------------------------------------*/
 extern double timediff(gtime_t t1, gtime_t t2)
 {
-    return difftime(t1.time,t2.time)+t1.sec-t2.sec;
+  return difftime(t1.time,t2.time)+t1.sec-t2.sec;
 }
 
 /* gps time to time ------------------------------------------------------------
@@ -116,12 +121,12 @@ extern double timediff(gtime_t t1, gtime_t t2)
 *-----------------------------------------------------------------------------*/
 gtime_t gpst2time(int week, double sec)
 {
-    gtime_t t=epoch2time(gpst0);
+  gtime_t t=epoch2time(gpst0);
 
-    if (sec<-1E9||1E9<sec) sec=0.0;
-    t.time+=86400*7*week+(int)sec;
-    t.sec=sec-(int)sec;
-    return t;
+  if (sec<-1E9||1E9<sec) sec=0.0;
+  t.time+=86400*7*week+(int)sec;
+  t.sec=sec-(int)sec;
+  return t;
 }
 
 /* gpstime to utc --------------------------------------------------------------
@@ -130,14 +135,25 @@ gtime_t gpst2time(int week, double sec)
 * return : time expressed in utc
 * notes  : ignore slight time offset under 100 ns
 *-----------------------------------------------------------------------------*/
-extern gtime_t gpst2utc(gtime_t t)
+gtime_t gpst2utc(gtime_t t)
 {
-    gtime_t tu;
-    size_t i;
+  gtime_t tu;
+  size_t i;
 
-    for (i=0;i<sizeof(leaps)/sizeof(*leaps);i++) {
-        tu=timeadd(t,leaps[i][6]);
-        if (timediff(tu,epoch2time(leaps[i]))>=0.0) return tu;
-    }
-    return t;
+  for (i=0;i<sizeof(leaps)/sizeof(*leaps);i++) {
+    tu=timeadd(t,leaps[i][6]);
+    if (timediff(tu,epoch2time(leaps[i]))>=0.0) return tu;
+  }
+  return t;
 }
+
+int64_t gps2UtcNSecs(int gps_week, double gps_secs)
+{
+  gtime_t unix_time = gpst2utc(
+        gpst2time(gps_week, gps_secs));
+  return static_cast<int64_t>(
+        ze::secToNanosec(
+          unix_time.sec+static_cast<double>(unix_time.time)));
+}
+
+} // ze namespace
