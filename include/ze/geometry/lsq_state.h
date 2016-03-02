66c67b4ae420779aaf14c96fd65ef0911ebdcf95
Christian Forster <ch.forster@gmail.com>
2016-03-02 13:56:13 +0100

    working on traits for State.

--- include/ze/geometry/lsq_state.h
+++ include/ze/geometry/lsq_state.h
@@ -2,38 +2,146 @@
 
 #include <tuple>
 #include <type_traits>
+#include <typeinfo>
 #include <iostream>
+#include <glog/logging.h>
+#include <ze/common/types.h>
 
 namespace ze {
 
+namespace internal {
+
+template<typename T> struct TupleGetDimension;
+
+template<typename Element>
+struct TupleGetDimension<std::tuple<Element>>
+{
+ static const unsigned int dim = traits<Element>::dimension;
+};
+
+template<typename Element, typename... Elements>
+struct TupleGetDimension<std::tuple<Element, Elements...>>
+{
+  static const unsigned int dim = traits<Element>::dimension
+           + TupleGetDimension<std::tuple<Elements...>>::dim;
+};
+
+} // namespace internal
+
+// -----------------------------------------------------------------------------
+// A State can contain fixed-sized elements such as Scalars, Vector3, Transformation.
 template<typename... Elements>
 class State
 {
 public:
-  typedef decltype(std::tuple<Elements...>()) state_t;
-  static const unsigned int Size = std::tuple_size<state_t>::value;
+  typedef State<Elements...> StateT;
+  typedef decltype(std::tuple<Elements...>()) StateTuple;
+  static const unsigned int size = std::tuple_size<StateTuple>::value;
+  static const unsigned int dimension = internal::TupleGetDimension<StateTuple>::dim;
 
-  state_t state_;
+  template <size_t i>
+  using ElementType = typename std::tuple_element<i, StateTuple>::type;
+
+  typedef Eigen::Matrix<FloatType, dimension, 1> TangentVector;
+  typedef Eigen::Matrix<FloatType, dimension, dimension> Jacobian;
+
+  StateTuple state_;
 
   void print() const
   {
-    std::cout << "(";
     printImpl<0>();
-    std::cout << std::endl;
+    std::cout << "--\n";
+  }
+
+  void retract(const TangentVector& v)
+  {
+    retractImpl<0,0>(v);
   }
 
 private:
-  template<unsigned int i, typename std::enable_if<(i<Size)>::type* = nullptr>
+  template<unsigned int i, typename std::enable_if<(i<size)>::type* = nullptr>
   inline void printImpl() const
   {
-    std::cout << std::get<i>(state_) << ", ";
+    using T = ElementType<i>;
+    std::cout << "--\nState-Index: " << i << "\n"
+              << "Type = " << typeid(T).name() << "\n"
+              << "Dimension = " << traits<T>::dimension << "\n"
+              << "Value = \n" << std::get<i>(state_) << "\n";
     printImpl<i+1>();
   }
 
-  template<unsigned int i, typename std::enable_if<(i>=Size)>::type* = nullptr>
+  template<unsigned int i, typename std::enable_if<(i>=size)>::type* = nullptr>
   inline void printImpl() const
   {}
+
+  template<unsigned int i=0, unsigned int j=0, typename std::enable_if<(i<size)>::type* = nullptr>
+  inline void retractImpl(const TangentVector& v)
+  {
+    static_assert(j < dimension, "");
+    using T = ElementType<i>;
+    //typename traits<T>::TangentVector t = v.template segment<traits<T>::dimension>(j);
+
+    std::get<i>(state_) = traits<T>::Retract(
+           std::get<i>(state_),
+           v.template segment<traits<T>::dimension>(j));
+    retractImpl<i+1, j+traits<T>::dimension>(v);
+  }
+
+  template<unsigned int i=0, unsigned int j=0, typename std::enable_if<(i>=size)>::type* = nullptr>
+  inline void retractImpl(const TangentVector& v)
+  {}
 };
+
+
+// -----------------------------------------------------------------------------
+// Manifold traits for fixed-size state.
+template<typename T> struct traits;
+
+template<typename... Elements>
+struct traits<State<Elements...>>
+{
+  typedef State<Elements...> StateT;
+  static const unsigned int dimension = StateT::dimension;
+
+  typedef Eigen::Matrix<FloatType, dimension, 1> TangentVector;
+  typedef Eigen::Matrix<FloatType, dimension, dimension> Jacobian;
+
+  static StateT Retract(const StateT& origin, const TangentVector& v,
+                      Jacobian* H1 = nullptr, Jacobian* H2 = nullptr)
+  {
+    if(H1 || H2)
+    {
+      LOG(FATAL) << "Not implemented.";
+    }
+    StateT origin_perturbed = origin;
+    origin_perturbed.retract(v);
+    return origin_perturbed;
+  }
+
+  /*
+  static bool Equals(const Matrix& v1, const Matrix& v2, double tol = 1e-8)
+  {
+    if (v1.size() != v2.size())
+      return false;
+    return (v1 - v2).array().abs().maxCoeff() < tol;
+    // TODO(cfo): Check for nan entries.
+  }
+
+  static TangentVector Local(Matrix origin, Matrix other,
+                            Jacobian* H1 = nullptr, Jacobian* H2 = nullptr)
+  {
+    if (H1) (*H1) = -Jacobian::Identity();
+    if (H2) (*H2) =  Jacobian::Identity();
+    TangentVector result;
+    Eigen::Map<Matrix>(result.data()) = other - origin;
+    return result;
+  }
+  */
+};
+
+
+
+
 
 
 
--- test/test_lsq_state.cpp
+++ test/test_lsq_state.cpp
@@ -1,18 +1,27 @@
 #include <random>
 #include <ze/common/test_entrypoint.h>
 #include <ze/common/matrix.h>
+#include <ze/common/manifold.h>
 #include <ze/common/types.h>
 #include <ze/common/transformation.h>
 #include <ze/geometry/lsq_state.h>
 
-int main(int argc, char** argv)
+TEST(StateTests, testRetract)
 {
   using namespace ze;
-  State<Transformation,Vector3> state;
+
+  using MyState = State<Transformation,Vector3,FloatType>;
+  EXPECT_EQ(MyState::dimension, 10);
+
+  MyState state;
   state.print();
 
+  MyState::TangentVector v;
+  state.retract(v);
+
+  EXPECT_EQ(State<Transformation>::dimension, 6);
 
   return 0;
 }
 
-//ZE_UNITTEST_ENTRYPOINT
+ZE_UNITTEST_ENTRYPOINT
