d0e851175ebff80d42bea37d4ba01f0f77e41798
Christian Forster <forster@ifi.uzh.ch>
2016-03-07 09:31:15 +0100

    refactor

--- include/ze/geometry/pose_optimizer.h
+++ include/ze/geometry/pose_optimizer.h
@@ -39,7 +39,9 @@ public:
       const FloatType prior_weight_pos, const FloatType prior_weight_rot);
 
   FloatType evaluateError(
-      const Transformation& T_B_W, HessianMatrix* H, GradientVector *g);
+      const Transformation& T_B_W,
+      HessianMatrix* H,
+      GradientVector* g);
 
 private:
   const std::vector<PoseOptimizerFrameData>& data_;
