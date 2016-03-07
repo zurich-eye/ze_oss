da72444d668edb17cfd971aedc3f9e7924d6be20
Christian Forster <forster@ifi.uzh.ch>
2016-03-07 18:45:15 +0100

    fix test (worked before...)

--- test/test_align_points.cpp
+++ test/test_align_points.cpp
@@ -19,10 +19,13 @@ TEST(AlignPointsTest, testJacobian)
   T_A_B.setRandom(1.0);
 
   auto residualLambda = [&](const Transformation& T_A_B) {
-      return p_A - T_A_B * p_B;
+      return Vector3(p_A - T_A_B * p_B);
     };
+
   Matrix36 J_numeric = numericalDerivative<Vector3, Transformation>(residualLambda, T_A_B);
   Matrix36 J_analytic = dPointdistance_dRelpose(T_A_B, p_A, p_B);
+
+  std::cout << "J_numeric: \n" << J_numeric << std::endl;
 
   EXPECT_TRUE(EIGEN_MATRIX_EQUAL_DOUBLE(J_numeric, J_analytic));
 }
