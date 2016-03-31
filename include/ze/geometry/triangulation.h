d1774b0ab5ef841d5c678fa701a2746f43948bff
Christian Forster <forster@ifi.uzh.ch>
2016-03-31 17:47:47 +0200

    apply all changes from swe integration.

--- include/ze/geometry/triangulation.h
+++ include/ze/geometry/triangulation.h
@@ -1,14 +1,17 @@
 #pragma once
 
+#include <tuple>
+
 #include <ze/common/types.h>
 #include <ze/common/transformation.h>
 
 namespace ze {
 
+//! Return depth in reference frame.
 inline std::pair<FloatType, bool> depthFromTriangulation(
     const Transformation& T_cur_ref,
-    const Eigen::Vector3d& f_ref,
-    const Eigen::Vector3d& f_cur)
+    const Eigen::Ref<const Bearing>& f_ref,
+    const Eigen::Ref<const Bearing>& f_cur)
 {
   Matrix32 A;
   A << T_cur_ref.getRotation().rotate(f_ref), f_cur;
@@ -20,5 +23,35 @@ inline std::pair<FloatType, bool> depthFromTriangulation(
   Vector2 depths = - AtA.inverse() * A.transpose() * T_cur_ref.getPosition();
   return std::make_pair(std::abs(depths(0)), true);
 }
+
+//! Compute the position of a 3D point seen from two viewpoints. Fast
+//! non-linear approximation (closed-form), as in Davide's book.
+//! The resulting point is expressed in coordinate frame A.
+Position triangulateNonLinear(
+    const Transformation& T_A_B,
+    const Eigen::Ref<const Bearing>& f_A,
+    const Eigen::Ref<const Bearing>& f_B);
+
+//! Triangulate multiple 3d points using triangulateNonLinear and compute
+//! the corresponding reprojection errors.
+void triangulateManyAndComputeAngularErrors(
+    const Transformation& T_A_B,
+    const Bearings& f_A_vec,
+    const Bearings& f_B_vec,
+    Positions& p_A,
+    VectorX& reprojection_erors);
+
+//! DLT triangulation [Hartley and Zisserman, 2nd edition, p. 312].
+//! @param T_C_W vector of camera poses (camera in world coordinates).
+//! @param f_C bearing vectors in camera frame.
+//! @param rank_tol SVD rank tolerance.
+//! @return Triangulated point, in homogeneous coordinates.
+//! @return Success.
+std::pair<Vector4, bool> triangulateHomogeneousDLT(
+    const TransformationVector& T_C_W,
+    const Bearings& f_C,
+    const FloatType rank_tol = 1e-9);
+
+
 
 } // namespace ze
