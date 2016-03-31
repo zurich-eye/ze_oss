d9f7d075747b32499d989202e555cfcef99ec021
Christian Forster <ch.forster@gmail.com>
2016-03-31 22:21:47 +0200

    fix cmake to enable multi-threading.

--- cmake/modules/ze_setup.cmake
+++ cmake/modules/ze_setup.cmake
@@ -1,6 +1,6 @@
 # Set build flags, set ARM_ARCHITECTURE environment variable on Odroid
 set(CMAKE_CXX_FLAGS "-pthread")
-set(CMAKE_CXX_FLAGS "-Wall -Werror -fPIC -D_REENTRANT -march=native -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unknown-pragmas")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Werror -fPIC -D_REENTRANT -march=native -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unknown-pragmas")
 
 if(DEFINED ENV{ARM_ARCHITECTURE})
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon -march=armv7-a")
@@ -10,9 +10,9 @@ else()
 endif()
 
 if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --std=c++11 -D_LINUX")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --std=c++11 -D_LINUX -lpthread")
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-  set(CMAKE_CXX_FLAGS "-std=c++11 -stdlib=libc++")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++")
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
   message(AUTHOR_WARNING "For now, we do not support Windows.")
 else()
