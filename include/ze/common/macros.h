4b4ca25c63ce85cd24ec29b59e2ab8ca0bd511f8
Christian Forster <forster@ifi.uzh.ch>
2016-03-31 15:58:46 +0200

    add brisk descriptor test.

--- include/ze/common/macros.h
+++ include/ze/common/macros.h
@@ -6,3 +6,7 @@
   typedef std::unique_ptr<TypeName> UniquePtr;      \
   typedef std::shared_ptr<TypeName> Ptr;            \
   typedef std::shared_ptr<const TypeName> ConstPtr
+
+#define ZE_DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName&) = delete;         \
+  void operator=(const TypeName&) = delete
