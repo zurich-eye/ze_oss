71dd342024dd57756b357b97c8731db3b42f5941
Manuel Werlberger <code@werlberger.org>
2016-04-10 22:04:00 +0200

    also testing other models

--- test/test_dense_variational_stereo.cpp
+++ test/test_dense_variational_stereo.cpp
@@ -85,7 +85,7 @@ TEST(DenseStereoTests, testVariationalStereoHuberL1)
   {
     ze::cu::cvBridgeShow("Reference Image", *cuimg_ref);
     ze::cu::cvBridgeShow("Current Image", *cuimg_cur);
-    ze::cu::cvBridgeShow("Disparities", *cudisp, true);
+    ze::cu::cvBridgeShow("Disparities Huber-L1", *cudisp, true);
     if (cuocc)
     {
       ze::cu::cvBridgeShow("Occlusions", *cuocc);
@@ -94,6 +94,125 @@ TEST(DenseStereoTests, testVariationalStereoHuberL1)
   }
 }
 
+//-----------------------------------------------------------------------------
+TEST(DenseStereoTests, testVariationalStereoPrecondHuberL1)
+{
+  using Stereo = ze::cu::VariationalStereo;
+  using StereoParameters = Stereo::Parameters;
+
+  // Load two images:
+  std::string data_path = ze::getTestDataDir("computer_vision_images");
+  ze::cu::ImageGpu32fC1::Ptr cuimg_ref, cuimg_cur;
+  ze::cu::cvBridgeLoad(cuimg_ref, data_path + "/cones/im2.png", ze::PixelOrder::gray);
+  ze::cu::cvBridgeLoad(cuimg_cur, data_path + "/cones/im6.png", ze::PixelOrder::gray);
+//  ze::cvBridgeLoad(gt_disp_ref, data_path + "/cones/disp2.png", ze::PixelOrder::gray);
+//  ze::cvBridgeLoad(gt_disp_cur, data_path + "/cones/disp6.png", ze::PixelOrder::gray);
+
+  {
+    ze::Pixel32fC1 min_val,max_val;
+    ze::cu::minMax(*cuimg_ref, min_val, max_val);
+    VLOG(2) << "disp: min: " << min_val.x << " max: " << max_val.x;
+  }
+
+  // compute dense stereo
+  StereoParameters::Ptr stereo_params = std::make_shared<StereoParameters>();
+  stereo_params->solver = ze::cu::StereoPDSolver::PrecondHuberL1;
+  stereo_params->ctf.scale_factor = 0.8f;
+  stereo_params->ctf.iters = 50;
+  stereo_params->ctf.warps  = 10;
+  stereo_params->ctf.apply_median_filter = true;
+
+  std::unique_ptr<Stereo> stereo(new Stereo(stereo_params));
+
+  stereo->addImage(cuimg_ref);
+  stereo->addImage(cuimg_cur);
+
+  stereo->solve();
+
+  ze::cu::ImageGpu32fC1::Ptr cudisp = stereo->getDisparities();
+  ze::cu::ImageGpu32fC1::Ptr cuocc = stereo->getOcclusion();
+
+  CHECK_NOTNULL(cudisp.get());
+
+  {
+    ze::Pixel32fC1 min_val,max_val;
+    ze::cu::minMax(*cudisp, min_val, max_val);
+    VLOG(2) << "disp: min: " << min_val.x << " max: " << max_val.x;
+  }
+
+
+  if (FLAGS_visualize)
+  {
+    ze::cu::cvBridgeShow("Reference Image", *cuimg_ref);
+    ze::cu::cvBridgeShow("Current Image", *cuimg_cur);
+    ze::cu::cvBridgeShow("Disparities Preconditioned Huber-L1", *cudisp, true);
+    if (cuocc)
+    {
+      ze::cu::cvBridgeShow("Occlusions", *cuocc);
+    }
+    cv::waitKey(0);
+  }
+}
+
+//-----------------------------------------------------------------------------
+TEST(DenseStereoTests, testVariationalStereoPrecondHuberL1Weighted)
+{
+  using Stereo = ze::cu::VariationalStereo;
+  using StereoParameters = Stereo::Parameters;
+
+  // Load two images:
+  std::string data_path = ze::getTestDataDir("computer_vision_images");
+  ze::cu::ImageGpu32fC1::Ptr cuimg_ref, cuimg_cur;
+  ze::cu::cvBridgeLoad(cuimg_ref, data_path + "/cones/im2.png", ze::PixelOrder::gray);
+  ze::cu::cvBridgeLoad(cuimg_cur, data_path + "/cones/im6.png", ze::PixelOrder::gray);
+//  ze::cvBridgeLoad(gt_disp_ref, data_path + "/cones/disp2.png", ze::PixelOrder::gray);
+//  ze::cvBridgeLoad(gt_disp_cur, data_path + "/cones/disp6.png", ze::PixelOrder::gray);
+
+  {
+    ze::Pixel32fC1 min_val,max_val;
+    ze::cu::minMax(*cuimg_ref, min_val, max_val);
+    VLOG(2) << "disp: min: " << min_val.x << " max: " << max_val.x;
+  }
+
+  // compute dense stereo
+  StereoParameters::Ptr stereo_params = std::make_shared<StereoParameters>();
+  stereo_params->solver = ze::cu::StereoPDSolver::PrecondHuberL1Weighted;
+  stereo_params->ctf.scale_factor = 0.8f;
+  stereo_params->ctf.iters = 50;
+  stereo_params->ctf.warps  = 10;
+  stereo_params->ctf.apply_median_filter = true;
+
+  std::unique_ptr<Stereo> stereo(new Stereo(stereo_params));
+
+  stereo->addImage(cuimg_ref);
+  stereo->addImage(cuimg_cur);
+
+  stereo->solve();
+
+  ze::cu::ImageGpu32fC1::Ptr cudisp = stereo->getDisparities();
+  ze::cu::ImageGpu32fC1::Ptr cuocc = stereo->getOcclusion();
+
+  CHECK_NOTNULL(cudisp.get());
+
+  {
+    ze::Pixel32fC1 min_val,max_val;
+    ze::cu::minMax(*cudisp, min_val, max_val);
+    VLOG(2) << "disp: min: " << min_val.x << " max: " << max_val.x;
+  }
+
+
+  if (FLAGS_visualize)
+  {
+    ze::cu::cvBridgeShow("Reference Image", *cuimg_ref);
+    ze::cu::cvBridgeShow("Current Image", *cuimg_cur);
+    ze::cu::cvBridgeShow("Disparities Preconditioned Huber-L1 Edge Weighted", *cudisp, true);
+    if (cuocc)
+    {
+      ze::cu::cvBridgeShow("Occlusions", *cuocc);
+    }
+    cv::waitKey(0);
+  }
+}
 
 
 ZE_UNITTEST_ENTRYPOINT
