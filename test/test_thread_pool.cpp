449011519fd381f09533e92d2b4fb4fdb8fe37d4
Christian Forster <forster@ifi.uzh.ch>
2016-04-21 08:11:50 +0200

    replace cout with VLOG

--- test/test_thread_pool.cpp
+++ test/test_thread_pool.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <chrono>
 
+#include <ze/common/logging.hpp>
 #include <ze/common/thread_pool.h>
 
 int main(int argc, char** argv)
@@ -13,9 +14,9 @@ int main(int argc, char** argv)
   {
     results.emplace_back(
       pool.enqueue([i] {
-        std::cout << "started job " << i << std::endl;
+        VLOG(1) << "started job " << i;
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        std::cout << "stopped job " << i << std::endl;
+        VLOG(1) << "stopped job " << i;
         return i*i;
       })
     );
