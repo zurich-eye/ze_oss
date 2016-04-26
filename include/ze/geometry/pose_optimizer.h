3f50b712a0240d552d7e1f5c629c1d9db8ee67fc
Christian Forster <forster@ifi.uzh.ch>
2016-04-26 21:29:07 +0200

    implement pose optimizer on unit plane.

--- include/ze/geometry/pose_optimizer.h
+++ include/ze/geometry/pose_optimizer.h
@@ -8,16 +8,25 @@
 
 namespace ze {
 
+enum class PoseOptimizerResidualType
+{
+  Bearing,
+  UnitPlane,
+  UnitPlaneEdgelet
+};
+
 //! Data required by the pose-optimizer per frame.
 struct PoseOptimizerFrameData
 {
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
+  PoseOptimizerResidualType type;
+
   //! Measurements: Bearing vectors corresponding to keypoints.
   Bearings f;
 
-  //! Measurements: Keypoints. (Not used by the actual algorithm).
-  Keypoints px;
+  //! Gradients: Used only for edgelets.
+  Gradients grad;
 
   //! Measurements bookkeeping: Corresponding indices. (Not used by the actual algorithm).
   KeypointIndices kp_idx;
@@ -28,6 +37,14 @@ struct PoseOptimizerFrameData
 
   //! Extrinsic transformation between camera and body (i.e., imu) frame.
   Transformation T_C_B;
+
+  //! @name Internal buffer
+  //! @{
+  //! Measurements: Projected on unit-plane. (computed internally).
+  Keypoints uv;
+
+  FloatType measurement_sigma;
+  //! @}
 };
 using PoseOptimizerFrameDataVec = std::vector<PoseOptimizerFrameData>;
 
@@ -42,7 +59,7 @@ public:
   using WeightFunction = TukeyWeightFunction<FloatType>;
 
   PoseOptimizer(
-      const std::vector<PoseOptimizerFrameData>& data,
+      std::vector<PoseOptimizerFrameData>& data,
       const Transformation& T_B_W_prior,
       const FloatType prior_weight_pos, const FloatType prior_weight_rot);
 
@@ -52,14 +69,27 @@ public:
       GradientVector* g);
 
 private:
-  const std::vector<PoseOptimizerFrameData>& data_;
-  std::vector<FloatType> measurement_sigma_;
+  std::vector<PoseOptimizerFrameData>& data_;
 
   // Prior:
   const Transformation& T_B_W_prior_;
   FloatType prior_weight_pos_;
   FloatType prior_weight_rot_;
 };
+
+FloatType evaluateBearingErrors(
+    const Transformation& T_B_W,
+    const bool compute_measurement_sigma,
+    PoseOptimizerFrameData& data,
+    PoseOptimizer::HessianMatrix* H,
+    PoseOptimizer::GradientVector* g);
+
+FloatType evaluateUnitPlaneErrors(
+    const Transformation& T_B_W,
+    const bool compute_measurement_sigma,
+    PoseOptimizerFrameData& data,
+    PoseOptimizer::HessianMatrix* H,
+    PoseOptimizer::GradientVector* g);
 
 /*!
  * @brief Jacobian of bearing vector w.r.t. landmark in camera coordinates.
@@ -70,7 +100,7 @@ private:
  * df/dp = 1 / (x^2 + y^2 + z^2)^(3/2) * | -xy, x^2 + z^2, -yz |
  *                                       | -xz, -yz, x^2 + z^2 |.
  */
-inline Matrix3 dBearing_dLandmark(const Position& p_C)
+inline Matrix3 dBearing_dLandmark(const Eigen::Ref<const Position>& p_C)
 {
   const FloatType x2 = p_C(0) * p_C(0);
   const FloatType y2 = p_C(1) * p_C(1);
@@ -84,6 +114,19 @@ inline Matrix3 dBearing_dLandmark(const Position& p_C)
        -xy, x2 + z2, -yz,
        -xz, -yz, x2 + y2;
   J /= (x2_y2_z2 * std::sqrt(x2_y2_z2));
+  return J;
+}
+
+/*!
+ * @brief Jacobian of unit-plane coordinates uv w.r.t. landmark in camera coordinates.
+ */
+inline Matrix23 dUv_dLandmark(const Eigen::Ref<const Position>& p_C)
+{
+  const FloatType z_sq = p_C(2) * p_C(2);
+  const FloatType z_inv = 1.0 / p_C(2);
+  Matrix23 J;
+  J << z_inv, 0.0, -p_C(0) / z_sq,
+       0.0, z_inv, -p_C(1) / z_sq;
   return J;
 }
 
--- src/pose_optimizer.cpp
+++ src/pose_optimizer.cpp
@@ -12,86 +12,35 @@
 namespace ze {
 
 PoseOptimizer::PoseOptimizer(
-    const std::vector<PoseOptimizerFrameData>& data,
+    std::vector<PoseOptimizerFrameData>& data,
     const Transformation& T_B_W_prior,
     const FloatType prior_weight_pos, const FloatType prior_weight_rot)
   : data_(data)
   , T_B_W_prior_(T_B_W_prior)
   , prior_weight_pos_(prior_weight_pos)
   , prior_weight_rot_(prior_weight_rot)
-{
-  // The scale of the error is computed at the first iteration.
-  measurement_sigma_.resize(data.size());
-}
+{}
 
 FloatType PoseOptimizer::evaluateError(
     const Transformation& T_B_W, HessianMatrix* H, GradientVector* g)
 {
-  CHECK_EQ(data_.size(), measurement_sigma_.size());
   FloatType chi2 = 0.0;
 
   // Loop over all cameras in rig.
-  for (size_t i = 0; i < data_.size(); ++i)
+  for (auto& residual_block : data_)
   {
-    const PoseOptimizerFrameData& data = data_[i];
-    FloatType& measurement_sigma = measurement_sigma_[i];
-
-    if (data.f.cols() == 0)
+    switch (residual_block.type)
     {
-      LOG(WARNING) << "Have no measurements in camera " << i;
-      continue;
+      case PoseOptimizerResidualType::Bearing:
+        chi2 += evaluateBearingErrors(T_B_W, iter_ == 0, residual_block, H, g);
+        break;
+      case PoseOptimizerResidualType::UnitPlane:
+        chi2 += evaluateUnitPlaneErrors(T_B_W, iter_ == 0, residual_block, H, g);
+        break;
+      default:
+        LOG(FATAL) << "Residual type not implemented.";
+        break;
     }
-
-    // Transform points from world coordinates to camera coordinates.
-    const Transformation T_C_W = data.T_C_B * T_B_W;
-    Positions p_C = T_C_W.transformVectorized(data.p_W);
-
-    // Normalize points to obtain estimated bearing vectors.
-    Bearings f_est = p_C;
-    normalizeBearings(f_est);
-
-    // Compute difference between bearing vectors.
-    Bearings f_err = f_est - data.f;
-    VectorX f_err_norm = f_err.colwise().norm();
-
-    // At the first iteration, compute the scale of the error.
-    if (iter_ == 0)
-    {
-      measurement_sigma = ScaleEstimator::compute(f_err_norm);
-      VLOG(300) << "Cam " << i << " measurement sigma = " << measurement_sigma;
-    }
-
-    // Robust cost function.
-    VectorX weights =
-        WeightFunction::weightVectorized(f_err_norm / measurement_sigma);
-
-    // Whiten error.
-    f_err /= measurement_sigma;
-
-    if (H && g)
-    {
-      const Matrix3 R_C_W = T_C_W.getRotationMatrix();
-      const int n = data.f.cols();
-      Matrix36 G;
-      G.block<3,3>(0,0) = I_3x3;
-      for(int i = 0; i < n; ++i)
-      {
-        // Jacobian computation.
-        G.block<3,3>(0,3) = - skewSymmetric(data.p_W.col(i));
-        Matrix3 J_normalization = dBearing_dLandmark(p_C.col(i));
-        Matrix36 J = J_normalization * R_C_W * G;
-
-        // Whiten Jacobian.
-        J /= measurement_sigma;
-
-        // Compute Hessian and Gradient Vector.
-        H->noalias() += J.transpose() * J * weights(i);
-        g->noalias() -= J.transpose() * f_err.col(i) * weights(i);
-      }
-    }
-
-    // Compute log-likelihood : 1/(2*sigma^2)*(z-h(x))^2 = 1/2*e'R'*R*e
-    chi2 += 0.5 * weights.dot(f_err.colwise().squaredNorm());
   }
 
   // Apply prior.
@@ -101,6 +50,128 @@ FloatType PoseOptimizer::evaluateError(
   }
 
   return chi2;
+}
+
+//------------------------------------------------------------------------------
+FloatType evaluateBearingErrors(
+    const Transformation& T_B_W,
+    const bool first_iteration,
+    PoseOptimizerFrameData& data,
+    PoseOptimizer::HessianMatrix* H,
+    PoseOptimizer::GradientVector* g)
+{
+  // Transform points from world coordinates to camera coordinates.
+  const Transformation T_C_W = data.T_C_B * T_B_W;
+  Positions p_C = T_C_W.transformVectorized(data.p_W);
+
+  // Normalize points to obtain estimated bearing vectors.
+  Bearings f_est = p_C;
+  normalizeBearings(f_est);
+
+  // Compute difference between bearing vectors.
+  Bearings f_err = f_est - data.f;
+  VectorX f_err_norm = f_err.colwise().norm();
+
+  // At the first iteration, compute the scale of the error.
+  if (first_iteration)
+  {
+    data.measurement_sigma = PoseOptimizer::ScaleEstimator::compute(f_err_norm);
+  }
+
+  // Robust cost function.
+  VectorX weights =
+      PoseOptimizer::WeightFunction::weightVectorized(f_err_norm / data.measurement_sigma);
+
+  // Whiten error.
+  f_err /= data.measurement_sigma;
+
+  if (H && g)
+  {
+    const Matrix3 R_C_W = T_C_W.getRotationMatrix();
+    const int n = data.f.cols();
+    Matrix36 G;
+    G.block<3,3>(0,0) = I_3x3;
+    for(int i = 0; i < n; ++i)
+    {
+      // Jacobian computation.
+      G.block<3,3>(0,3) = -skewSymmetric(data.p_W.col(i));
+      Matrix3 J_normalization = dBearing_dLandmark(p_C.col(i));
+      Matrix36 J = J_normalization * R_C_W * G;
+
+      // Whiten Jacobian.
+      J /= data.measurement_sigma;
+
+      // Compute Hessian and Gradient Vector.
+      H->noalias() += J.transpose() * J * weights(i);
+      g->noalias() -= J.transpose() * f_err.col(i) * weights(i);
+    }
+  }
+
+  // Compute log-likelihood : 1/(2*sigma^2)*(z-h(x))^2 = 1/2*e'R'*R*e
+  return 0.5 * weights.dot(f_err.colwise().squaredNorm());
+}
+
+//------------------------------------------------------------------------------
+FloatType evaluateUnitPlaneErrors(
+    const Transformation& T_B_W,
+    const bool first_iteration,
+    PoseOptimizerFrameData& data,
+    PoseOptimizer::HessianMatrix* H,
+    PoseOptimizer::GradientVector* g)
+{
+  if (first_iteration)
+  {
+    data.uv = project2Vectorized(data.f);
+  }
+
+  // Transform points from world coordinates to camera coordinates.
+  const Transformation T_C_W = data.T_C_B * T_B_W;
+  Positions p_C = T_C_W.transformVectorized(data.p_W);
+
+  // Compute projection of points on unit-plane:
+  Keypoints uv_est = project2Vectorized(p_C);
+
+  // Compute difference between bearing vectors.
+  Keypoints uv_err = uv_est - data.uv;
+  VectorX uv_err_norm = uv_err.colwise().norm();
+
+  // At the first iteration, compute the scale of the error.
+  if (first_iteration)
+  {
+    data.measurement_sigma = PoseOptimizer::ScaleEstimator::compute(uv_err_norm);
+  }
+
+  // Robust cost function.
+  VectorX weights =
+      PoseOptimizer::WeightFunction::weightVectorized(uv_err_norm / data.measurement_sigma);
+
+  // Whiten error.
+  uv_err /= data.measurement_sigma;
+
+  if (H && g)
+  {
+    const Matrix3 R_C_W = T_C_W.getRotationMatrix();
+    const int n = data.f.cols();
+    Matrix36 G;
+    G.block<3,3>(0,0) = I_3x3;
+    for(int i = 0; i < n; ++i)
+    {
+      // Jacobian computation.
+      G.block<3,3>(0,3) = -skewSymmetric(data.p_W.col(i));
+      Matrix23 J_proj = dUv_dLandmark(p_C.col(i));
+      Matrix26 J = J_proj * R_C_W * G;
+
+      // Whiten Jacobian.
+      J /= data.measurement_sigma;
+
+      // Compute Hessian and Gradient Vector.
+      H->noalias() += J.transpose() * J * weights(i);
+      g->noalias() -= J.transpose() * uv_err.col(i) * weights(i);
+    }
+  }
+
+  // Compute log-likelihood : 1/(2*sigma^2)*(z-h(x))^2 = 1/2*e'R'*R*e
+  return 0.5 * weights.dot(uv_err.colwise().squaredNorm());
 }
 
 } // namespace ze
