9ec15f2d11e69ec4de36d947a23c84d6b5997f59
Manuel Werlberger <code@werlberger.org>
2016-04-29 10:31:28 +0200

    code review

--- include/ze/geometry/epipolar_geometry.hpp
+++ include/ze/geometry/epipolar_geometry.hpp
@@ -16,16 +16,14 @@ inline Matrix3 essentialMatrix(const Transformation& T)
 
 // ----------------------------------------------------------------------------
 //! Compute fundamental matrix from given camera transformation
-//! @note Function assumes a camera rig with two cameras and cam0 recording the
-//!       reference image and cam1 the current (moving) image
 inline Matrix3 fundamentalMatrix(const Transformation& T_cam0_cam1,
-                                 const ze::CameraRig& cam_rig)
+                                 const ze::VectorX& projection_parameteres0,
+                                 const ze::VectorX& projection_parameteres1)
 {
-  CHECK(cam_rig.size() == 2);
-  Matrix3 K0, K1;
-  const ze::VectorX projection_parameteres0 = cam_rig.at(0).projectionParameters();
-  const ze::VectorX projection_parameteres1 = cam_rig.at(1).projectionParameters();
+  CHECK(projection_parameteres0.size() >= 4);
+  CHECK(projection_parameteres1.size() >= 4);
 
+  Matrix3 K0, K1;
   K0 << projection_parameteres0(0), 0, projection_parameteres0(2),
       0, projection_parameteres0(1), projection_parameteres0(3),
       0, 0, 1;
@@ -34,21 +32,6 @@ inline Matrix3 fundamentalMatrix(const Transformation& T_cam0_cam1,
       0, 0, 1;
 
   return (K0.inverse().transpose() * essentialMatrix(T_cam0_cam1) * K1.inverse());
-}
-
-// ----------------------------------------------------------------------------
-//! Compute fundamental matrix from given transformation and camera (e.g. monocular moving)
-inline Matrix3 fundamentalMatrix(const Transformation& T,
-                                 const ze::Camera& cam)
-{
-  Matrix3 K;
-  const ze::VectorX projection_parameteres = cam.projectionParameters();
-
-  K << projection_parameteres(0), 0, projection_parameteres(2),
-      0, projection_parameteres(1), projection_parameteres(3),
-      0, 0, 1;
-
-  return (K.inverse().transpose() * essentialMatrix(T) * K.inverse());
 }
 
 } // namespace ze
