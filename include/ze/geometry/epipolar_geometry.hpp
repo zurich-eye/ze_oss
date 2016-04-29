383c5b34216c31ddde329b6fe3859a07d992ddcd
Manuel Werlberger <code@werlberger.org>
2016-04-29 09:14:29 +0200

    changed licenses tbd->ZE

--- include/ze/geometry/epipolar_geometry.hpp
+++ include/ze/geometry/epipolar_geometry.hpp
@@ -36,4 +36,21 @@ inline Matrix3 fundamentalMatrix(const Transformation& T_cam0_cam1,
   return (K0.inverse().transpose() * essentialMatrix(T_cam0_cam1) * K1.inverse().transpose());
 }
 
+// ----------------------------------------------------------------------------
+//! Compute fundamental matrix from given camera transformation
+//! @note Function assumes a camera rig with two cameras and cam0 recording the
+//!       reference image and cam1 the current (moving) image
+inline Matrix3 fundamentalMatrix(const Transformation& T_ref_cur,
+                                 const ze::Camera& cam)
+{
+  Matrix3 K;
+  const ze::VectorX projection_parameteres = cam.projectionParameters();
+
+  K << projection_parameteres(0), 0, projection_parameteres(2),
+      0, projection_parameteres(1), projection_parameteres(3),
+      0, 0, 1;
+
+  return (K.inverse().transpose() * essentialMatrix(T_ref_cur) * K.inverse().transpose());
+}
+
 } // namespace ze
