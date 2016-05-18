db0e3a1ca14e9c8735d79853f04ce92e93db09a2
Christian Forster <forster@ifi.uzh.ch>
2016-05-18 11:15:51 +0200

    implemented common stereo interface.

--- include/ze/geometry/epipolar_geometry.hpp
+++ include/ze/geometry/epipolar_geometry.hpp
@@ -11,27 +11,27 @@ namespace ze {
 //! Compute essential matrix from given camera transformation
 inline Matrix3 essentialMatrix(const Transformation& T)
 {
-  return ze::skewSymmetric(T.getPosition()) * T.getRotationMatrix();
+  return skewSymmetric(T.getPosition()) * T.getRotationMatrix();
 }
 
 // ----------------------------------------------------------------------------
 //! Compute fundamental matrix from given camera transformation
-inline Matrix3 fundamentalMatrix(const Transformation& T_cam0_cam1,
-                                 const ze::VectorX& projection_parameteres0,
-                                 const ze::VectorX& projection_parameteres1)
+inline Matrix3 fundamentalMatrix(const Transformation& T_ref_cur,
+                                 const VectorX& proj_params_ref,
+                                 const VectorX& proj_params_cur)
 {
-  CHECK(projection_parameteres0.size() >= 4);
-  CHECK(projection_parameteres1.size() >= 4);
+  CHECK_GE(proj_params_ref.size(), 4u);
+  CHECK_GE(proj_params_cur.size(), 4u);
 
-  Matrix3 K0, K1;
-  K0 << projection_parameteres0(0), 0, projection_parameteres0(2),
-      0, projection_parameteres0(1), projection_parameteres0(3),
-      0, 0, 1;
-  K1 << projection_parameteres1(0), 0, projection_parameteres1(2),
-      0, projection_parameteres1(1), projection_parameteres1(3),
-      0, 0, 1;
+  Matrix3 K_ref, K_cur;
+  K_ref << proj_params_ref(0), 0, proj_params_ref(2),
+           0, proj_params_ref(1), proj_params_ref(3),
+           0, 0, 1;
+  K_cur << proj_params_cur(0), 0, proj_params_cur(2),
+           0, proj_params_cur(1), proj_params_cur(3),
+           0, 0, 1;
 
-  return (K0.inverse().transpose() * essentialMatrix(T_cam0_cam1) * K1.inverse());
+  return (K_ref.inverse().transpose() * essentialMatrix(T_ref_cur) * K_cur.inverse());
 }
 
 } // namespace ze
