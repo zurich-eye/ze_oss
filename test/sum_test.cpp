479c2b3ef1e07a0662ee0be752d38def9cbb46bc
Matia Pizzoli <matia.pizzoli@gmail.com>
2016-05-19 14:41:28 +0200

    added test for cuda sum

---
+++ test/sum_test.cpp
@@ -0,0 +1,91 @@
+#include <gtest/gtest.h>
+
+// system includes
+#include <assert.h>
+#include <cstdint>
+#include <cfloat>
+#include <iostream>
+#include <random>
+#include <functional>
+#include <limits>
+#include <imp/core/image_raw.hpp>
+#include <imp/cu_core/cu_math.cuh>
+#include <imp/cu_core/cu_utils.hpp>
+
+template<class T>
+typename std::enable_if<std::is_integral<T>::value, std::function<T()> >::type
+getRandomGenerator()
+{
+  std::default_random_engine generator;
+  std::uniform_int_distribution<T> distribution(std::numeric_limits<T>::lowest(),
+                                                std::numeric_limits<T>::max());
+  auto random_val = std::bind(distribution, generator);
+  return random_val;
+}
+
+template<class T>
+typename std::enable_if<!std::is_integral<T>::value, std::function<T()> >::type
+getRandomGenerator()
+{
+  std::default_random_engine generator;
+  std::uniform_real_distribution<T> distribution(FLT_MIN,
+                                                 std::numeric_limits<T>::max());
+  auto random_val = std::bind(distribution, generator);
+  return random_val;
+}
+
+TEST(IMPCuCoreTestSuite,sumTest_8uC1)
+{
+  // setup random number generator
+  auto random_val = getRandomGenerator<std::uint8_t>();
+
+  const size_t width = 752;
+  const size_t height = 480;
+  ze::ImageRaw8uC1 im(width,height);
+
+  double gt_sum = 0.0;
+  for (size_t y=0; y<height; ++y)
+  {
+    for (size_t x=0; x<width; ++x)
+    {
+      std::uint8_t random_value = random_val();
+      im[y][x] = random_value;
+      gt_sum += im.pixel(x, y);
+    }
+  }
+  IMP_CUDA_CHECK();
+  ze::cu::ImageGpu8uC1 cu_im(im);
+  IMP_CUDA_CHECK();
+  double cu_sum = ze::cu::sum(cu_im);
+  EXPECT_FLOAT_EQ(gt_sum, cu_sum);
+  VLOG(2) << "GT sum: " << gt_sum;
+  VLOG(2) << "GPU sum: " << cu_sum;
+}
+
+TEST(IMPCuCoreTestSuite,sumTest_32fC1)
+{
+  // setup random number generator
+  auto random_val = getRandomGenerator<float>();
+
+  const size_t width = 752;
+  const size_t height = 480;
+  ze::ImageRaw32fC1 im(width,height);
+
+  double gt_sum = 0.0;
+  for (size_t y=0; y<height; ++y)
+  {
+    for (size_t x=0; x<width; ++x)
+    {
+      float random_value = random_val();
+      im[y][x] = random_value;
+      gt_sum += im.pixel(x, y);
+    }
+  }
+  IMP_CUDA_CHECK();
+  ze::cu::ImageGpu32fC1 cu_im(im);
+  IMP_CUDA_CHECK();
+  double cu_sum = ze::cu::sum(cu_im);
+  EXPECT_FLOAT_EQ(gt_sum, cu_sum);
+  VLOG(2) << "GT sum: " << gt_sum;
+  VLOG(2) << "GPU sum: " << cu_sum;
+}
