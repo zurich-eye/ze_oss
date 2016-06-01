58a74ebc830e6b4fb8559b613eb1ed5b240c0efc
Christian Forster <forster@ifi.uzh.ch>
2016-06-01 08:25:26 +0200

    working on random numbers.

--- include/ze/common/sample.h
+++ include/ze/common/sample.h
@@ -1,24 +1,94 @@
 #pragma once
 
 #include <random>
-#include <chrono>
-#include <Eigen/Core>
+#include <ze/common/types.h>
+#include <ze/common/noncopyable.hpp>
+
 
 namespace ze {
 
-class Sample
+//! Get deterministic or non-deterministc random number generator. Singleton.
+class RandomGenerator : Noncopyable
 {
 public:
-  static void setTimeBasedSeed();
+  static std::ranlux24 generatorReal(bool deterministic = false);
+  static std::mt19937  generatorInt(bool deterministic = false);
 
-  static int uniform(int from, int to);
-  static double uniform();
-  static double gaussian(double sigma);
-  static Eigen::Vector3d randomDirection3D();
-  static Eigen::Vector2d randomDirection2D();
+private:
+  RandomGenerator();
 
-  static std::ranlux24 gen_real;
-  static std::mt19937 gen_int;
+  static RandomGenerator& instance();
+
+  std::ranlux24 gen_real_;
+  std::mt19937 gen_int_;
+  std::ranlux24 gen_real_deterministic_;
+  std::mt19937 gen_int_deterministic_;
 };
+
+
+template<class T>
+typename std::enable_if<std::is_integral<T>::value, std::function<T()> >::type
+sampleFromUniformDistribution(
+    T from = std::numeric_limits<T>::lowest(),
+    T to   = std::numeric_limits<T>::max(),
+    bool deterministic = false)
+{
+  auto gen = RandomGenerator::generatorInt(deterministic);
+  auto dist = std::uniform_int_distribution<T>(from, to);
+  return dist(gen);
+}
+
+/*
+
+FloatType sampleFromUniformDistribution();
+
+double gaussian(double sigma);
+
+//! @return 3-dimensional unit vector.
+Vector3 randomDirection3D();
+
+//! @return 2-dimensional unit vector.
+Vector2 randomDirection2D();
+
+// ----------------------------------------------------------------------------
+//! Random number generator for integral types.
+//! @return Random number in interval [lowest, highest]
+template<class T>
+typename std::enable_if<std::is_integral<T>::value, std::function<T()> >::type
+getRandomGenerator()
+{
+  std::mt19937 generator(std::random_device{}());
+  std::uniform_int_distribution<T> distribution(std::numeric_limits<T>::lowest(),
+                                                std::numeric_limits<T>::max());
+  auto random_val = std::bind(distribution, generator);
+  return random_val;
+}
+
+// ----------------------------------------------------------------------------
+//! Random number generator for real types.
+//! @return Random number in interval [0.0, 1.0]
+template<class T>
+typename std::enable_if<!std::is_integral<T>::value, std::function<T()> >::type
+getRandomGenerator()
+{
+  std::mt19937 generator(std::random_device{}());
+  //! @todo (MWE) enable a min/max rnd generator - not working yet so using [0.0,1.0] for now
+  std::uniform_real_distribution<T> distribution(0.0, 1.0);
+//  std::numeric_limits<T>::lowest(),
+//      std::numeric_limits<T>::max());
+  auto random_val = std::bind(distribution, generator);
+  return random_val;
+}
+
+// ----------------------------------------------------------------------------
+//! Random number generator that returns a FloatType between 0.0 and 1.0.
+inline std::function<FloatType()> getRandomGenerator01()
+{
+  std::mt19937 generator(std::random_device{}());
+  std::uniform_real_distribution<FloatType> distribution(FloatType{0.0}, FloatType{1.0});
+  std::function<FloatType()> random_val = std::bind(distribution, generator);
+  return random_val;
+}
+*/
 
 } // namespace ze
--- src/sample.cpp
+++ src/sample.cpp
@@ -2,17 +2,31 @@
 
 namespace ze {
 
-std::ranlux24 Sample::gen_real;
-std::mt19937 Sample::gen_int;
+RandomGenerator::RandomGenerator()
+  : gen_real_(std::random_device{}())
+  , gen_int_(std::random_device{}())
+  , gen_real_deterministic_(0)
+  , gen_int_deterministic_(0)
+{}
 
-void Sample::setTimeBasedSeed()
+RandomGenerator& RandomGenerator::instance()
 {
-  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-  gen_real = std::ranlux24(seed);
-  gen_int = std::mt19937(seed);
+  static RandomGenerator gen;
+  return gen;
 }
 
-int Sample::uniform(int from, int to)
+std::ranlux24 RandomGenerator::generatorReal(bool deterministic)
+{
+  return deterministic ? instance().gen_real_deterministic_ : instance().gen_real_;
+}
+
+std::mt19937 RandomGenerator::generatorInt(bool deterministic)
+{
+  return deterministic ? instance().gen_int_deterministic_ : instance().gen_int_;
+}
+
+/*
+int sampleFromUniformDistribution(int from, int to)
 {
   std::uniform_int_distribution<int> distribution(from, to);
   return distribution(gen_int);
@@ -24,28 +38,29 @@ double Sample::uniform()
   return distribution(gen_real);
 }
 
-double Sample::gaussian(double stddev)
+double gaussian(double stddev)
 {
   std::normal_distribution<double> distribution(0.0, stddev);
   return distribution(gen_real);
 }
 
-Eigen::Vector3d Sample::randomDirection3D()
+Vector3 randomDirection3D()
 {
   // equal-area projection according to:
   // https://math.stackexchange.com/questions/44689/how-to-find-a-random-axis-or-unit-vector-in-3d
-  const double z = Sample::uniform()*2.0-1.0;
-  const double t = Sample::uniform()*2.0*M_PI;
-  const double r = std::sqrt(1.0 - z*z);
-  const double x = r*std::cos(t);
-  const double y = r*std::sin(t);
-  return Eigen::Vector3d(x,y,z);
+  const FloatType z = Sample::uniform() * 2.0 - 1.0;
+  const FloatType t = Sample::uniform() * 2.0 * M_PI;
+  const FloatType r = std::sqrt(1.0 - z*z);
+  const FloatType x = r * std::cos(t);
+  const FloatType y = r * std::sin(t);
+  return Vector3(x,y,z);
 }
 
-Eigen::Vector2d Sample::randomDirection2D()
+Vector2 randomDirection2D()
 {
   const double theta = Sample::uniform()*2.0*M_PI;
   return Eigen::Vector2d(std::cos(theta), std::sin(theta));
 }
+*/
 
 } // namespace ze
