d270eec060d96d8c962177a78fb81f456caa187b
Luc Oth <luc.oth@gmail.com>
2016-06-02 11:22:47 +0200

    Improved parameter naming and consistency

--- include/ze/common/sampler.hpp
+++ include/ze/common/sampler.hpp
@@ -17,41 +17,40 @@ public:
   typedef Eigen::Matrix<FloatType, DIM, 1> covariance_vector_t;
   typedef Eigen::Matrix<FloatType, DIM, 1> noise_vector_t;
 
-  //! Takes a diagonal covariance matrix.
-  GaussianSampler(const covariance_matrix_t& Sigma) : Sigma_(Sigma.diagonal())
-  {
-    // ensure a diagonal matrix
-    for (size_t i = 0; i < DIM; ++i)
-    {
-      for (size_t j = 0; j < DIM; ++j)
-      {
-        if (i != j)
-        {
-          CHECK_EQ(0, Sigma_(i, j));
-        }
-      }
-    }
-  }
-
-  //! Takes a vector containig the diagonal elements of a covariance matrix.
-  GaussianSampler(const covariance_vector_t& Sigma_vector)
-    : Sigma_(Sigma_vector)
-  {
-  }
-
   //! Get a noise sample.
   noise_vector_t sample()
   {
     noise_vector_t noise;
     for (size_t i = 0; i < DIM; ++i)
     {
-      noise(i) = Sample::gaussian(Sigma_(i));
+      // The gaussian takes a standard deviation as input.
+      noise(i) = Sample::gaussian(sigma_(i));
     }
     return noise;
   }
 
+  static Ptr sigmas(const covariance_vector_t& sigmas)
+  {
+    Ptr noise(new GaussianSampler);
+    noise->sigma_ = sigmas;
+
+    return noise;
+  }
+
+  static Ptr variances(const covariance_vector_t& variances)
+  {
+    Ptr noise(new GaussianSampler);
+    noise->sigma_ = variances.cwiseSqrt();
+
+    return noise;
+  }
+
+protected:
+  //! Takes a vector containig the diagonal elements of a covariance matrix.
+  GaussianSampler() {}
+
 private:
-  covariance_vector_t Sigma_;
+  covariance_vector_t sigma_;
 };
 
 } // namespace ze
--- test/test_sampler.cpp
+++ test/test_sampler.cpp
@@ -1,17 +1,17 @@
 #include <ze/common/test_entrypoint.h>
-#include <ze/common/sampler.h>
+#include <ze/common/sampler.hpp>
 
 TEST(SamplerTest, testSampler)
 {
   using namespace ze;
 
-  Matrix2 Sigma;
-  Sigma << 2, 0, 0, 3;
-  GaussianSampler<2> sampler(Sigma);
-  GaussianSampler<3> sampler2(Vector3(1, 2, 3));
+  Vector2 var_vector;
+  var_vector << 2, 3;
+  GaussianSampler<2>::Ptr sampler = GaussianSampler<2>::variances(var_vector);
+  GaussianSampler<3>::Ptr sampler2 = GaussianSampler<3>::sigmas(Vector3(1, 2, 3));
 
-  Vector2 sample = sampler.sample();
-  Vector3 sample2 = sampler2.sample();
+  Vector2 sample = sampler->sample();
+  Vector3 sample2 = sampler2->sample();
 }
 
 ZE_UNITTEST_ENTRYPOINT
