e2fa4cce1ece3476222192a0651ac55ff3402912
Luc Oth <luc.oth@gmail.com>
2016-06-03 17:46:36 +0200

    Separate package for matplotlib

---
+++ src/matplotlibcpp.cpp
@@ -0,0 +1,1 @@
+#include <ze/matplotlib/matplotlibcpp.hpp>
---
+++ src/mpl_example_node.cpp
@@ -0,0 +1,25 @@
+#include <ze/matplotlib/matplotlibcpp.hpp>
+
+namespace plt = matplotlibcpp;
+int main()
+{
+  using namespace ze;
+
+  // Simple:
+  std::vector<double> v({1, 2, 3, 4});
+  plt::plot(v);
+  plt::show();
+
+  // Eigen Vector Types:
+  VectorX times(100);
+  times.setLinSpaced(100, 0, 20);
+  VectorX points(100);
+  points.setRandom();
+
+  plt::plot(times, points);
+  plt::show();
+
+  plt::named_plot("A Name", times, points);
+  plt::show();
+
+}
