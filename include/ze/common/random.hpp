cd3668930d3053af9b742cea5f00a9f797ac7830
Christian Forster <forster@ifi.uzh.ch>
2016-06-06 08:51:57 +0200

    continued working on random numbers.

--- include/ze/common/random.hpp
+++ include/ze/common/random.hpp
@@ -1,33 +1,15 @@
 #pragma once
 
 #include <random>
+#include <ze/common/logging.hpp>
 #include <ze/common/types.h>
-#include <ze/common/noncopyable.hpp>
-
 
 namespace ze {
 
-//! Get deterministic or non-deterministc random number generator. Singleton.
-class RandomGenerator : Noncopyable
-{
-public:
-  static std::ranlux24 generatorReal(bool deterministic = false);
-  static std::mt19937  generatorInt(bool deterministic = false);
-
-private:
-  RandomGenerator();
-
-  static RandomGenerator& instance();
-
-  std::ranlux24 gen_real_;
-  std::mt19937 gen_int_;
-  std::ranlux24 gen_real_deterministic_;
-  std::mt19937 gen_int_deterministic_;
-};
-
-
-template<class T, typename = typename std::enable_if<std::is_integral<T>::value>::type>
-T sampleFromUniformDistribution(
+//------------------------------------------------------------------------------
+//! @return Sample from integer-valued distribution.
+template<typename T>
+T sampleFromUniformIntDistribution(
     bool deterministic = false,
     T from = std::numeric_limits<T>::lowest(),
     T to   = std::numeric_limits<T>::max())
@@ -35,12 +17,13 @@ T sampleFromUniformDistribution(
   static std::mt19937 gen_nondeterministic(std::random_device{}());
   static std::mt19937 gen_deterministic(0);
   auto dist = std::uniform_int_distribution<T>(from, to);
-  T random_number = deterministic ? dist(gen_deterministic) : dist(gen_nondeterministic);
-  return random_number;
+  return deterministic ? dist(gen_deterministic) : dist(gen_nondeterministic);
 }
 
-template<class T, typename = typename std::enable_if<!std::is_integral<T>::value>::type>
-T sampleFromUniformDistribution(
+//------------------------------------------------------------------------------
+//! @return Sample from uniform real-valued distribution.
+template<typename T>
+T sampleFromUniformRealDistribution(
     bool deterministic = false,
     T from = T{0.0},
     T to   = T{1.0})
@@ -48,15 +31,39 @@ T sampleFromUniformDistribution(
   static std::ranlux24 gen_nondeterministic(std::random_device{}());
   static std::ranlux24 gen_deterministic(0);
   auto dist = std::uniform_real_distribution<T>(from, to);
-  T random_number = deterministic ? dist(gen_deterministic) : dist(gen_nondeterministic);
-  return random_number;
+  return deterministic ? dist(gen_deterministic) : dist(gen_nondeterministic);
 }
 
-/*
+//------------------------------------------------------------------------------
+//! @return Sample from normal distribution (real-valued).
+template<typename T>
+T sampleFromNormalDistribution(
+    bool deterministic = false,
+    T mean = T{0.0},
+    T sigma   = T{1.0})
+{
+  static std::ranlux24 gen_nondeterministic(std::random_device{}());
+  static std::ranlux24 gen_deterministic(0);
+  auto dist = std::normal_distribution<T>(mean, sigma);
+  return deterministic ? dist(gen_deterministic) : dist(gen_nondeterministic);
+}
 
-FloatType sampleFromUniformDistribution();
+//------------------------------------------------------------------------------
+//! @return true with given probability. Samples the Bernoulli distribution.
+bool flipCoin(
+    bool deterministic = false,
+    FloatType true_probability = FloatType{0.5})
+{
+  DEBUG_CHECK_GE(true_probability, 0.0);
+  DEBUG_CHECK_LT(true_probability, 1.0);
+  static std::mt19937 gen_nondeterministic(std::random_device{}());
+  static std::mt19937 gen_deterministic(0);
+  auto dist = std::bernoulli_distribution(true_probability);
+  return deterministic ? dist(gen_deterministic) : dist(gen_nondeterministic);
+}
 
-double gaussian(double sigma);
+//------------------------------------------------------------------------------
+// Sample manifolds:
 
 //! @return 3-dimensional unit vector.
 Vector3 randomDirection3D();
@@ -64,18 +71,23 @@ Vector3 randomDirection3D();
 //! @return 2-dimensional unit vector.
 Vector2 randomDirection2D();
 
+
 // ----------------------------------------------------------------------------
-//! Random number generator for integral types.
-//! @return Random number in interval [lowest, highest]
+//! Usage: f = uniformIntDistribution(); sample = f();
+//! @return Uniform integer distribution in interval [from, to].
 template<class T>
-typename std::enable_if<std::is_integral<T>::value, std::function<T()> >::type
-getRandomGenerator()
+std::function<T()> uniformIntDistribution(
+    bool deterministic = false,
+    T from = std::numeric_limits<T>::lowest(),
+    T to   = std::numeric_limits<T>::max())
 {
-  std::mt19937 generator(std::random_device{}());
-  std::uniform_int_distribution<T> distribution(std::numeric_limits<T>::lowest(),
-                                                std::numeric_limits<T>::max());
-  auto random_val = std::bind(distribution, generator);
-  return random_val;
+  static std::mt19937 gen_nondeterministic(std::random_device{}());
+  static std::mt19937 gen_deterministic(0);
+  std::uniform_int_distribution<T> distribution(from, to);
+  auto fun = deterministic ?
+               std::bind(distribution, gen_deterministic) :
+               std::bind(distribution, gen_nondeterministic);
+  return fun;
 }
 
 // ----------------------------------------------------------------------------
@@ -103,6 +115,5 @@ inline std::function<FloatType()> getRandomGenerator01()
   std::function<FloatType()> random_val = std::bind(distribution, generator);
   return random_val;
 }
-*/
 
 } // namespace ze
--- src/random.cpp
+++ src/random.cpp
@@ -2,65 +2,25 @@
 
 namespace ze {
 
-RandomGenerator::RandomGenerator()
-  : gen_real_(std::random_device{}())
-  , gen_int_(std::random_device{}())
-  , gen_real_deterministic_(0)
-  , gen_int_deterministic_(0)
-{}
-
-RandomGenerator& RandomGenerator::instance()
-{
-  static RandomGenerator gen;
-  return gen;
-}
-
-std::ranlux24 RandomGenerator::generatorReal(bool deterministic)
-{
-  return deterministic ? instance().gen_real_deterministic_ : instance().gen_real_;
-}
-
-std::mt19937 RandomGenerator::generatorInt(bool deterministic)
-{
-  return deterministic ? instance().gen_int_deterministic_ : instance().gen_int_;
-}
-
-/*
-int sampleFromUniformDistribution(int from, int to)
-{
-  std::uniform_int_distribution<int> distribution(from, to);
-  return distribution(gen_int);
-}
-
-double Sample::uniform()
-{
-  std::uniform_real_distribution<double> distribution(0.0, 1.0);
-  return distribution(gen_real);
-}
-
-double gaussian(double stddev)
-{
-  std::normal_distribution<double> distribution(0.0, stddev);
-  return distribution(gen_real);
-}
-
-Vector3 randomDirection3D()
+Vector3 randomDirection3D(bool deterministic)
 {
   // equal-area projection according to:
   // https://math.stackexchange.com/questions/44689/how-to-find-a-random-axis-or-unit-vector-in-3d
-  const FloatType z = Sample::uniform() * 2.0 - 1.0;
-  const FloatType t = Sample::uniform() * 2.0 * M_PI;
-  const FloatType r = std::sqrt(1.0 - z*z);
+  const FloatType z =
+      sampleFromUniformRealDistribution(deterministic, 0.0, 1.0) * 2.0 - 1.0;
+  const FloatType t =
+      sampleFromUniformRealDistribution(deterministic, 0.0, 1.0) * 2.0 * M_PI;
+  const FloatType r = std::sqrt(1.0 - z * z);
   const FloatType x = r * std::cos(t);
   const FloatType y = r * std::sin(t);
-  return Vector3(x,y,z);
+  return Vector3(x, y, z);
 }
 
-Vector2 randomDirection2D()
+Vector2 randomDirection2D(bool deterministic)
 {
-  const double theta = Sample::uniform()*2.0*M_PI;
-  return Eigen::Vector2d(std::cos(theta), std::sin(theta));
+  const FloatType theta =
+      sampleFromUniformRealDistribution(deterministic, 0.0, 1.0) * 2.0 * M_PI;
+  return Vector2(std::cos(theta), std::sin(theta));
 }
-*/
 
 } // namespace ze
