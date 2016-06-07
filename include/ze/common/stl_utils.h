03dfa5f3896084c566341cc9c6760bb4771b20d9
Christian Forster <forster@ifi.uzh.ch>
2016-06-07 17:12:12 +0200

    make feature detection more generic for N cameras.

--- include/ze/common/stl_utils.h
+++ include/ze/common/stl_utils.h
@@ -5,6 +5,7 @@
 
 namespace ze {
 
+// -----------------------------------------------------------------------------
 //! Transform Eigen::Vector to std::vector.
 template <typename DerivedVec>
 std::vector<typename DerivedVec::Scalar> eigenVectorToStlVector(
@@ -18,6 +19,18 @@ std::vector<typename DerivedVec::Scalar> eigenVectorToStlVector(
     rv[i] = v(i);
   }
   return rv;
+}
+
+// -----------------------------------------------------------------------------
+//! @return Returns a vector of indices form 0 to stop.
+std::vector<uint32_t> range(uint32_t stop)
+{
+  std::vector<uint32_t> vec(stop);
+  for (uint32_t i = 0u; i < stop; ++i)
+  {
+    vec[i] = i;
+  }
+  return vec;
 }
 
 } // namespace ze
