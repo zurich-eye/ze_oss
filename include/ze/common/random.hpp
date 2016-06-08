547fca8b3ab1fc89fffc2a61b91bba0d43f933c7
Christian Forster <forster@ifi.uzh.ch>
2016-06-08 08:15:55 +0200

    added test_random and updated unit tests.

--- include/ze/common/random.hpp
+++ include/ze/common/random.hpp
@@ -28,8 +28,8 @@ T sampleFromUniformRealDistribution(
     T from = T{0.0},
     T to   = T{1.0})
 {
-  static std::ranlux24 gen_nondeterministic(std::random_device{}());
-  static std::ranlux24 gen_deterministic(0);
+  static std::mt19937 gen_nondeterministic(std::random_device{}());
+  static std::mt19937 gen_deterministic(0);
   auto dist = std::uniform_real_distribution<T>(from, to);
   return deterministic ? dist(gen_deterministic) : dist(gen_nondeterministic);
 }
@@ -42,15 +42,15 @@ T sampleFromNormalDistribution(
     T mean = T{0.0},
     T sigma   = T{1.0})
 {
-  static std::ranlux24 gen_nondeterministic(std::random_device{}());
-  static std::ranlux24 gen_deterministic(0);
+  static std::mt19937 gen_nondeterministic(std::random_device{}());
+  static std::mt19937 gen_deterministic(0);
   auto dist = std::normal_distribution<T>(mean, sigma);
   return deterministic ? dist(gen_deterministic) : dist(gen_nondeterministic);
 }
 
 //------------------------------------------------------------------------------
 //! @return true with given probability. Samples the Bernoulli distribution.
-bool flipCoin(
+inline bool flipCoin(
     bool deterministic = false,
     FloatType true_probability = FloatType{0.5})
 {
@@ -71,49 +71,58 @@ Vector3 randomDirection3D();
 //! @return 2-dimensional unit vector.
 Vector2 randomDirection2D();
 
+// -----------------------------------------------------------------------------
+// Get distributions, slightly faster than the above functions when many
+// random numbers are desired.
 
-// ----------------------------------------------------------------------------
-//! Usage: f = uniformIntDistribution(); sample = f();
+//! Usage: f = uniformDistribution<int>(); sample = f();
 //! @return Uniform integer distribution in interval [from, to].
 template<class T>
-std::function<T()> uniformIntDistribution(
+typename std::enable_if<std::is_integral<T>::value, std::function<T()> >::type
+uniformDistribution(
     bool deterministic = false,
     T from = std::numeric_limits<T>::lowest(),
     T to   = std::numeric_limits<T>::max())
 {
-  static std::mt19937 gen_nondeterministic(std::random_device{}());
-  static std::mt19937 gen_deterministic(0);
   std::uniform_int_distribution<T> distribution(from, to);
   auto fun = deterministic ?
-               std::bind(distribution, gen_deterministic) :
-               std::bind(distribution, gen_nondeterministic);
+               std::bind(distribution, std::mt19937(0)) :
+               std::bind(distribution, std::mt19937(std::random_device{}()));
   return fun;
 }
 
-// ----------------------------------------------------------------------------
-//! Random number generator for real types.
-//! @return Random number in interval [0.0, 1.0]
+// -----------------------------------------------------------------------------
+//! Usage: f = uniformDistribution<double>(); sample = f();
+//! @return Uniform real-valued distribution in interval [from, to].
 template<class T>
 typename std::enable_if<!std::is_integral<T>::value, std::function<T()> >::type
-getRandomGenerator()
+uniformDistribution(
+    bool deterministic = false,
+    T from = T{0.0},
+    T to   = T{1.0})
 {
-  std::mt19937 generator(std::random_device{}());
-  //! @todo (MWE) enable a min/max rnd generator - not working yet so using [0.0,1.0] for now
-  std::uniform_real_distribution<T> distribution(0.0, 1.0);
-//  std::numeric_limits<T>::lowest(),
-//      std::numeric_limits<T>::max());
-  auto random_val = std::bind(distribution, generator);
-  return random_val;
+  std::uniform_real_distribution<T> distribution(from, to);
+  auto fun = deterministic ?
+               std::bind(distribution, std::mt19937(0)) :
+               std::bind(distribution, std::mt19937(std::random_device{}()));
+  return fun;
 }
 
-// ----------------------------------------------------------------------------
-//! Random number generator that returns a FloatType between 0.0 and 1.0.
-inline std::function<FloatType()> getRandomGenerator01()
+// -----------------------------------------------------------------------------
+//! Usage: f = uniformDistribution<double>(); sample = f();
+//! @return Uniform real-valued distribution in interval [from, to].
+template<class T>
+std::function<T()>
+normalDistribution(
+    bool deterministic = false,
+    T mean  = T{0.0},
+    T sigma = T{1.0})
 {
-  std::mt19937 generator(std::random_device{}());
-  std::uniform_real_distribution<FloatType> distribution(FloatType{0.0}, FloatType{1.0});
-  std::function<FloatType()> random_val = std::bind(distribution, generator);
-  return random_val;
+  std::normal_distribution<T> distribution(mean, sigma);
+  auto fun = deterministic ?
+               std::bind(distribution, std::mt19937(0)) :
+               std::bind(distribution, std::mt19937(std::random_device{}()));
+  return fun;
 }
 
 } // namespace ze
---
+++ test/test_random.cpp
@@ -0,0 +1,113 @@
+#include <ze/common/benchmark.h>
+#include <ze/common/test_entrypoint.h>
+#include <ze/common/random.hpp>
+#include <ze/common/running_statistics.h>
+
+TEST(RandomTests, testRandomSampling)
+{
+  using namespace ze;
+
+  // Deterministic sampling results always the same series of random numbers.
+  EXPECT_EQ(sampleFromUniformIntDistribution<uint8_t>(true), 140u);
+  EXPECT_EQ(sampleFromUniformIntDistribution<uint8_t>(true), 151u);
+  EXPECT_EQ(sampleFromUniformIntDistribution<uint8_t>(true), 183u);
+
+  EXPECT_EQ(sampleFromUniformIntDistribution<int>(true), 209652396);
+  EXPECT_EQ(sampleFromUniformIntDistribution<int>(true), 398764591);
+  EXPECT_EQ(sampleFromUniformIntDistribution<int>(true), 924231285);
+
+  EXPECT_NEAR(sampleFromUniformRealDistribution<double>(true), 0.592844, 1e-5);
+  EXPECT_NEAR(sampleFromUniformRealDistribution<double>(true), 0.844265, 1e-5);
+  EXPECT_NEAR(sampleFromUniformRealDistribution<double>(true), 0.857945, 1e-5);
+
+  EXPECT_NEAR(sampleFromNormalDistribution<double>(true, 1.0, 4.0), 5.4911797, 1e-5);
+  EXPECT_NEAR(sampleFromNormalDistribution<double>(true, 1.0, 4.0), 1.2834369, 1e-5);
+  EXPECT_NEAR(sampleFromNormalDistribution<double>(true, 1.0, 4.0), -4.689303, 1e-5);
+
+  EXPECT_TRUE (flipCoin(true, 0.7));
+  EXPECT_FALSE(flipCoin(true, 0.7));
+  EXPECT_FALSE(flipCoin(true, 0.7));
+  EXPECT_FALSE(flipCoin(true, 0.7));
+  EXPECT_TRUE (flipCoin(true, 0.7));
+  EXPECT_TRUE (flipCoin(true, 0.7));
+
+  // Non-deterministic sampling, always results in different numbers:
+  EXPECT_NE(sampleFromUniformIntDistribution<int>(false), 209652396);
+  EXPECT_NE(sampleFromUniformIntDistribution<int>(false), 398764591);
+  EXPECT_NE(sampleFromUniformIntDistribution<int>(false), 924231285);
+
+  // Test mean and standard deviation of normal distribution.
+  {
+    RunningStatistics statistics;
+    for (int i = 0; i < 10000; ++i)
+    {
+      statistics.addSample(sampleFromNormalDistribution<double>(false, 2.0, 5.0));
+    }
+    EXPECT_NEAR(statistics.mean(), 2.0, 0.2);
+    EXPECT_NEAR(statistics.std(),  5.0, 0.2);
+  }
+
+  // Test coin flips.
+  {
+    RunningStatistics statistics;
+    for (int i = 0; i < 10000; ++i)
+    {
+      statistics.addSample(static_cast<int>(flipCoin(false, 0.2)));
+    }
+    EXPECT_NEAR(statistics.mean(), 0.2, 0.2);
+  }
+}
+
+TEST(RandomTests, testDistribution)
+{
+  using namespace ze;
+
+  // Deterministic sampling results always the same series of random numbers.
+  {
+    auto f = uniformDistribution<uint8_t>(true);
+    EXPECT_EQ(f(), 140u);
+    EXPECT_EQ(f(), 151u);
+    EXPECT_EQ(f(), 183u);
+  }
+
+  // Deterministic sampling results always the same series of random numbers.
+  {
+    auto f = uniformDistribution<double>(true, 1.0, 2.0);
+    EXPECT_NEAR(f(), 1.59284, 1e-5);
+    EXPECT_NEAR(f(), 1.84427, 1e-5);
+    EXPECT_NEAR(f(), 1.85795, 1e-5);
+  }
+
+  // Deterministic sampling results always the same series of random numbers.
+  {
+    auto f = normalDistribution<float>(true, 3.0, 5.0);
+    EXPECT_NEAR(f(), 14.06103, 1e-5);
+    EXPECT_NEAR(f(), 8.81539, 1e-5);
+    EXPECT_NEAR(f(), 6.87001, 1e-5);
+  }
+}
+
+TEST(RandomTests, benchmark)
+{
+  using namespace ze;
+
+  auto lambda1 = [&]()
+  {
+    int sum = 0;
+    for (int i = 0; i < 100000; ++i)
+      sum += sampleFromUniformIntDistribution<uint8_t>(false);
+  };
+  runTimingBenchmark(lambda1, 10, 10, "sampleSeparately", true);
+
+  auto lambda2 = [&]()
+  {
+    int sum = 0;
+    auto dist = uniformDistribution<uint8_t>(false);
+    for (int i = 0; i < 100000; ++i)
+      sum += dist();
+  };
+  runTimingBenchmark(lambda2, 10, 10, "sampleFromDistribution", true);
+}
+
+
+ZE_UNITTEST_ENTRYPOINT
