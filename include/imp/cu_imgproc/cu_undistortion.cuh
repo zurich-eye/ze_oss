3262b3e9bb524468f91d356f71a4ff8ae4e0b4f9
Matia Pizzoli <matia.pizzoli@gmail.com>
2016-06-09 20:34:23 +0200

    small refactoring

--- include/imp/cu_imgproc/cu_undistortion.cuh
+++ include/imp/cu_imgproc/cu_undistortion.cuh
@@ -6,21 +6,23 @@
 namespace ze {
 namespace cu {
 
-template <typename CameraModel,
-          typename DistortionModel,
-          typename T>
+template<typename CameraModel,
+         typename DistortionModel,
+         typename Pixel>
 class ImageUndistorter
 {
 public:
-  ImageUndistorter(int32_t width, int32_t height, T* camera_params, T* dist_coeffs);
-  ~ImageUndistorter();
-  void undistort(const ImageGpu32fC1& in, ImageGpu32fC1& out);
+  ImageUndistorter(
+      Size2u img_size,
+      const Eigen::RowVectorXf& camera_params,
+      const Eigen::RowVectorXf& dist_coeffs);
+  ~ImageUndistorter() = default;
+  void undistort(
+      const ImageGpu<Pixel>& src,
+      ImageGpu<Pixel>& dst);
 
 private:
   ImageGpu32fC2 undistortion_map_;
-  T* d_cam_params_;
-  T* d_dist_coeffs_;
-
   Fragmentation<> fragm_;
 };
 
