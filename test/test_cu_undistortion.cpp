0cce9e2bb68559b85d33989d353c66595a6d7d9a
Matia Pizzoli <matia.pizzoli@gmail.com>
2016-06-10 13:15:12 +0200

    added test for radtan, zero distortion

--- test/test_cu_undistortion.cpp
+++ test/test_cu_undistortion.cpp
@@ -8,10 +8,8 @@
 
 using namespace ze;
 
-TEST(impCuUndistortion, pinholeEquidistant_32fC1_Texture)
+TEST(impCuUndistortionTexture, equidist32fC1_zeroDistortion)
 {
-  using PinholeEquidistUndistorter = cu::ImageUndistorter<cu::PinholeGeometry, cu::EquidistantDistortion, Pixel32fC1>;
-
   const std::string test_data_name{"ze_feature_detection"};
   const std::string predefined_img_data_file_name{"752x480/pyr_0.png"};
 
@@ -27,14 +25,14 @@ TEST(impCuUndistortion, pinholeEquidistant_32fC1_Texture)
 
   Eigen::RowVectorXf cam_params(4);
   cam_params << 471.690643292, 471.765601046, 371.087464172, 228.63874151;
-
   Eigen::RowVectorXf dist_coeff(4);
-  dist_coeff << 0.00676530475436, -0.000811126898338, 0.0166458761987, -0.0172655346139;
+  dist_coeff << 0, 0, 0, 0;
 
-  ze::cu::ImageGpu32fC1 gpu_src(*cv_img);
-  ze::cu::ImageGpu32fC1 gpu_dst(cv_img->size());
+  cu::ImageGpu32fC1 gpu_src(*cv_img);
+  cu::ImageGpu32fC1 gpu_dst(cv_img->size());
 
-  PinholeEquidistUndistorter undistorter(gpu_src.size(), cam_params, dist_coeff);
+  cu::RadTanUndistort32fC1 undistorter(
+        gpu_src.size(), cam_params, dist_coeff);
   undistorter.undistort(gpu_src, gpu_dst);  // GPU warm-up
   auto undistortLambda = [&](){
     undistorter.undistort(gpu_src, gpu_dst);
@@ -42,14 +40,14 @@ TEST(impCuUndistortion, pinholeEquidistant_32fC1_Texture)
   runTimingBenchmark(
         undistortLambda, 10, 20,
         "CUDA undistortion using Textures", true);
-  ze::ImageCv32fC1 cv_img_out(gpu_dst);
-  //! @todo (MPI) find a baseline and compare
+
+  ImageCv32fC1 cv_img_out(gpu_dst);
   for (uint32_t y = 0; y < cv_img_out.height(); ++y)
   {
     for (uint32_t x = 0; x < cv_img_out.width(); ++x)
     {
-      EXPECT_LE(cv_img_out.cvMat().at<float>(y, x), 1.0f);
-      EXPECT_GE(cv_img_out.cvMat().at<float>(y, x), 0.0f);
+      EXPECT_FLOAT_EQ(cv_img->cvMat().at<float>(y, x),
+                      cv_img_out.cvMat().at<float>(y, x));
     }
   }
 }
