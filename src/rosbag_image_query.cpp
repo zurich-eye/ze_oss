6e5f08076a911782ebb89cef1c3c7160af76b3d1
Christian Forster <forster@ifi.uzh.ch>
2016-06-10 09:27:47 +0200

    u.

--- src/rosbag_image_query.cpp
+++ src/rosbag_image_query.cpp
@@ -1,33 +1,5 @@
 // Heavily modified by ze.
-// Copyright (c) 2016, Robotics and Perception Group, Titus Cieslewski
-// All Rights Reserved
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//
-// 1. Redistributions of source code must retain the above copyright notice,
-// this list of conditions and the following disclaimer.
-//
-// 2. Redistributions in binary form must reproduce the above copyright notice,
-// this list of conditions and the following disclaimer in the documentation and
-// or other materials provided with the distribution.
-//
-// 3. Neither the name of the copyright holder nor the names of its contributors
-// may be used to endorse or promote products derived from this software without
-// specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-// SUCH DAMAGE.
-
+// RPG-BSD License, Titus Cieslewski
 #include <ze/ros/rosbag_image_query.hpp>
 
 #include <glog/logging.h>
@@ -37,17 +9,25 @@
 
 namespace ze {
 
-RosbagImageQuery::RosbagImageQuery(const std::string& bag_file)
+RosbagImageQuery::RosbagImageQuery(const std::string& bagfile_path)
 {
-  CHECK(fileExists(bag_file)) << "File does not exist: " << bag_file;
+  CHECK(loadRosbag(bagfile_path));
+}
+
+bool RosbagImageQuery::loadRosbag(const std::string& bagfile_path)
+{
+  CHECK(fileExists(bagfile_path)) << "File does not exist: " << bagfile_path;
   try
   {
-    bag_.open(bag_file, rosbag::BagMode::Read);
+    VLOG(1) << "Opening rosbag " << bagfile_path << " ...";
+    bag_.open(bagfile_path, rosbag::BagMode::Read);
   }
   catch (rosbag::BagException& exception)
   {
-    LOG(FATAL) << "Could not open rosbag: " << bag_file << ": " << exception.what();
+    LOG(ERROR) << "Could not open rosbag: " << bagfile_path << ": " << exception.what();
+    return false;
   }
+  return true;
 }
 
 StampedImage RosbagImageQuery::getStampedImageAtTime(
@@ -60,8 +40,7 @@ StampedImage RosbagImageQuery::getStampedImageAtTime(
   ros::Time time_min, time_max;
   time_min.fromNSec(stamp_ns - search_range_ns);
   time_max.fromNSec(stamp_ns + search_range_ns);
-  rosbag::View view(
-      bag_, rosbag::TopicQuery(img_topic), time_min, time_max);
+  rosbag::View view(bag_, rosbag::TopicQuery(img_topic), time_min, time_max);
 
   VLOG(100) << "Found messages that fit = " << view.size();
   int64_t best_time_diff = std::numeric_limits<int64_t>::max();
