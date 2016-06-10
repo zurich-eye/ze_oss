1118492e41e09e399e358ed9efbd581fbf541b16
Matia Pizzoli <matia.pizzoli@gmail.com>
2016-06-10 09:32:41 +0200

    pinhole camera functions replicated for the gpu. I don't like it we need to figure out how to use the same code.

--- include/imp/cu_core/cu_pinhole_camera.cuh
+++ include/imp/cu_core/cu_pinhole_camera.cuh
@@ -8,6 +8,167 @@
 namespace ze {
 namespace cu {
 
+// Pinhole projection model.
+struct PinholeGeometry
+{
+  template <typename T>
+  __device__
+  static void project(const T* params, T* px)
+  {
+    const T fx = params[0];
+    const T fy = params[1];
+    const T cx = params[2];
+    const T cy = params[3];
+    px[0] = px[0] * fx + cx;
+    px[1] = px[1] * fy + cy;
+  }
+
+  template <typename T>
+  __device__
+  static void backProject(const T* params, T* px)
+  {
+    const T fx = params[0];
+    const T fy = params[1];
+    const T cx = params[2];
+    const T cy = params[3];
+    px[0] = (px[0] - cx) / fx;
+    px[1] = (px[1] - cy) / fy;
+  }
+};
+
+enum class DistortionType
+{
+  No,
+  Fov,
+  RadTan,
+  Equidistant,
+};
+
+// -----------------------------------------------------------------------------
+// This class implements the distortion model described in the paper:
+// "A Generic Camera Model and Calibration Method for Conventional, Wide-Angle,
+// and Fish-Eye Lenses" by Juho Kannala and Sami S. Brandt, PAMI.
+struct EquidistantDistortion
+{
+  static constexpr DistortionType type = DistortionType::Equidistant;
+
+  template <typename T>
+  __device__
+  static void distort(const T* params, T* px, T* jac_colmajor = nullptr)
+  {
+    const T x = px[0];
+    const T y = px[1];
+    const T k1 = params[0];
+    const T k2 = params[1];
+    const T k3 = params[2];
+    const T k4 = params[3];
+    const T r_sqr = x * x + y * y;
+    const T r = std::sqrt(r_sqr);
+    const T theta = std::atan(r);
+    const T theta2 = theta * theta;
+    const T theta4 = theta2 * theta2;
+    const T theta6 = theta4 * theta2;
+    const T theta8 = theta4 * theta4;
+    const T thetad = theta * (1.0 + k1 * theta2 + k2 * theta4 + k3 * theta6 + k4 * theta8);
+    const T scaling = (r > 1e-8) ? thetad / r : 1.0;
+    px[0] *= scaling;
+    px[1] *= scaling;
+
+    if (jac_colmajor)
+    {
+      T& J_00 = jac_colmajor[0];
+      T& J_10 = jac_colmajor[1];
+      T& J_01 = jac_colmajor[2];
+      T& J_11 = jac_colmajor[3];
+
+      if(r < 1e-7)
+      {
+        J_00 = 1.0; J_01 = 0.0;
+        J_10 = 0.0; J_11 = 1.0;
+      }
+      else
+      {
+        T xx = x * x;
+        T yy = y * y;
+        T xy = x * y;
+        T theta_inv_r = theta / r;
+        T theta_sqr = theta * theta;
+        T theta_four = theta_sqr * theta_sqr;
+
+        T t1 = 1.0 / (xx + yy + 1.0);
+        T t2 = k1 * theta_sqr
+            + k2 * theta_four
+            + k3 * theta_four * theta_sqr
+            + k4 * (theta_four * theta_four) + 1.0;
+        T t3 = t1 * theta_inv_r;
+
+        T offset = t2 * theta_inv_r;
+        T scale  = t2 * (t1 / r_sqr - theta_inv_r / r_sqr)
+            + theta_inv_r * t3 * (
+              2.0 * k1
+              + 4.0 * k2 * theta_sqr
+              + 6.0 * k3 * theta_four
+              + 8.0 * k4 * theta_four * theta_sqr);
+
+        J_11 = yy * scale + offset;
+        J_00 = xx * scale + offset;
+        J_01 = xy * scale;
+        J_10 = J_01;
+      }
+    }
+  }
+
+  template <typename T>
+  __device__
+  static void undistort(const T* params, T* px)
+  {
+    const T k1 = params[0];
+    const T k2 = params[1];
+    const T p1 = params[2];
+    const T p2 = params[3];
+
+    T jac_colmajor[4];
+    T x[2];
+    T x_tmp[2];
+    x[0] = px[0]; x[1]= px[1];
+    for(int i = 0; i < 30; ++i)
+    {
+      x_tmp[0] = x[0]; x_tmp[1] = x[1];
+      distort(params, x_tmp, jac_colmajor);
+
+      const T e_u = px[0] - x_tmp[0];
+      const T e_v = px[1] - x_tmp[1];
+
+      const T a = jac_colmajor[0];
+      const T b = jac_colmajor[1];
+      const T d = jac_colmajor[3];
+
+      // direct gauss newton step
+      const T a_sqr = a * a;
+      const T b_sqr = b * b;
+      const T d_sqr = d * d;
+      const T abbd = a * b + b * d;
+      const T abbd_sqr = abbd * abbd;
+      const T a2b2 = a_sqr + b_sqr;
+      const T a2b2_inv = 1.0 / a2b2;
+      const T adabdb = a_sqr * d_sqr - 2 * a * b_sqr * d + b_sqr * b_sqr;
+      const T adabdb_inv = 1.0 / adabdb;
+      const T c1 = abbd * adabdb_inv;
+
+      x[0] += e_u * (a * (abbd_sqr * a2b2_inv * adabdb_inv + a2b2_inv) - b * c1) + e_v * (b * (abbd_sqr * a2b2_inv * adabdb_inv + a2b2_inv) - d * c1);
+      x[1] += e_u * (-a * c1 + b * a2b2 * adabdb_inv) + e_v * (-b * c1 + d * a2b2 * adabdb_inv);
+
+      if ((e_u * e_u + e_v * e_v) < 1e-8)
+      {
+        break;
+      }
+    }
+
+    px[0] = x[0];
+    px[1] = x[1];
+  }
+};
+
 /**
  * @brief The PinholeCamera class implements a very simple pinhole camera model.
  *        Points in the image plane are denoted by the coordinate vectors (u,v) whereas
@@ -88,20 +249,20 @@ public:
   }
 
 
-//  __host__ __device__ __forceinline__
-//  Vec32fC3 cam2world(const Vec32fC2& uv) const
-//  {
-//    return Vec32fC3((uv.x-c_.x)/f_.x,
-//                    (uv.y-c_.y)/f_.y,
-//                    1.0f);
-//  }
+  //  __host__ __device__ __forceinline__
+  //  Vec32fC3 cam2world(const Vec32fC2& uv) const
+  //  {
+  //    return Vec32fC3((uv.x-c_.x)/f_.x,
+  //                    (uv.y-c_.y)/f_.y,
+  //                    1.0f);
+  //  }
 
-//  __host__ __device__ __forceinline__
-//  Vec32fC2 world2cam(const Vec32fC3& p) const
-//  {
-//    return Vec32fC2(f_.x*p.x/p.z + c_.x,
-//                    f_.y*p.y/p.z + c_.y);
-//  }
+  //  __host__ __device__ __forceinline__
+  //  Vec32fC2 world2cam(const Vec32fC3& p) const
+  //  {
+  //    return Vec32fC2(f_.x*p.x/p.z + c_.x,
+  //                    f_.y*p.y/p.z + c_.y);
+  //  }
 
   __host__ __device__ __forceinline__
   float3 cam2world(const float2& uv) const
