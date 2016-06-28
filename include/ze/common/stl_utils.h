cf71b9baa7c8d28e248e27f5c95a4378901a84f5
Christian Forster <forster@ifi.uzh.ch>
2016-06-28 23:03:21 +0200

    working on code review.

--- include/ze/common/stl_utils.h
+++ include/ze/common/stl_utils.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <numeric>
 #include <vector>
 #include <ze/common/types.h>
 #include <ze/common/logging.hpp>
@@ -23,28 +24,20 @@ std::vector<typename DerivedVec::Scalar> eigenVectorToStlVector(
 }
 
 // -----------------------------------------------------------------------------
-//! @return Returns a vector of indices form 0 to stop.
-inline std::vector<uint32_t> range(uint32_t stop)
-{
-  std::vector<uint32_t> vec(stop);
-  for (uint32_t i = 0u; i < stop; ++i)
-  {
-    vec[i] = i;
-  }
-  return vec;
-}
-
-// -----------------------------------------------------------------------------
 //! @return Returns a vector of indices form start to stop.
 inline std::vector<uint32_t> range(uint32_t start, uint32_t stop)
 {
   DEBUG_CHECK_GE(stop, start);
   std::vector<uint32_t> vec(stop - start);
-  for (uint32_t j = start, k = 0u; j < stop; ++j, ++k)
-  {
-    vec[k] = j;
-  }
+  std::iota(vec.begin(), vec.end(), start);
   return vec;
+}
+
+// -----------------------------------------------------------------------------
+//! @return Returns a vector of indices form 0 to stop.
+inline std::vector<uint32_t> range(uint32_t stop)
+{
+  return range(0u, stop);
 }
 
 } // namespace ze
