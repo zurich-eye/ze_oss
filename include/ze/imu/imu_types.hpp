5e67a984c23071dfc1d924381094406bfa7ea1c7
Luc Oth <luc.oth@gmail.com>
2016-07-12 10:11:16 +0200

    Better return types of imu_types getAcc, getGyr

--- include/ze/imu/imu_types.hpp
+++ include/ze/imu/imu_types.hpp
@@ -5,13 +5,25 @@
 namespace ze {
 
 //! Extract the Accelerometer component of an ImuAccGyr block.
-inline Vector3 getAcc(const ImuAccGyr& imu)
+inline Eigen::VectorBlock<const Vector6, 3> getAcc(const ImuAccGyr& imu)
 {
-  return imu.head<3>();
+    return imu.head<3>();
 }
 
 //! Extract the Gyroscope component of an ImuAccGyr block.
-inline Vector3 getGyr(const ImuAccGyr& imu)
+inline Eigen::VectorBlock<const Vector6, 3> getGyr(const ImuAccGyr& imu)
+{
+  return imu.tail<3>();
+}
+
+//! Extract the Accelerometer component of an ImuAccGyr block.
+inline Eigen::VectorBlock<Vector6, 3> getAcc(ImuAccGyr& imu)
+{
+    return imu.head<3>();
+}
+
+//! Extract the Gyroscope component of an ImuAccGyr block.
+inline Eigen::VectorBlock<Vector6, 3> getGyr(ImuAccGyr& imu)
 {
   return imu.tail<3>();
 }
