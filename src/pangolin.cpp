b8e139cfae500ede110f35d3b1e315d507a01b76
Luc Oth <luc.oth@gmail.com>
2016-07-27 14:23:50 +0200

    Missing line

--- src/pangolin.cpp
+++ src/pangolin.cpp
@@ -31,7 +31,6 @@ void PangolinPlotter::loop()
 
   pangolin::DisplayBase().AddDisplay(*plotter_);
 
-  while(!pangolin::ShouldQuit())
   while(!isStopRequested())
   {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
