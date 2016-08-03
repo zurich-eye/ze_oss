51743e13f07a3e94c4ee1cb6b4860e68a2d989c8
Matia Pizzoli <matia.pizzoli@gmail.com>
2016-08-03 17:51:30 +0200

    started coding rectification for equidistant

--- include/ze/geometry/epipolar_geometry.hpp
+++ include/ze/geometry/epipolar_geometry.hpp
@@ -11,27 +11,42 @@ namespace ze {
 //! Compute essential matrix from given camera transformation
 inline Matrix3 essentialMatrix(const Transformation& T)
 {
-  return ze::skewSymmetric(T.getPosition()) * T.getRotationMatrix();
+  return skewSymmetric(T.getPosition()) * T.getRotationMatrix();
 }
 
 // ----------------------------------------------------------------------------
 //! Compute fundamental matrix from given camera transformation
 inline Matrix3 fundamentalMatrix(const Transformation& T_cam0_cam1,
-                                 const ze::VectorX& projection_parameteres0,
-                                 const ze::VectorX& projection_parameteres1)
+                                 const VectorX& projection_parameters0,
+                                 const VectorX& projection_parameters1)
 {
-  CHECK_EQ(projection_parameteres0.size(), 4u);
-  CHECK_EQ(projection_parameteres1.size(), 4u);
+  CHECK_EQ(projection_parameters0.size(), 4u);
+  CHECK_EQ(projection_parameters1.size(), 4u);
 
   Matrix3 K0, K1;
-  K0 << projection_parameteres0(0), 0, projection_parameteres0(2),
-      0, projection_parameteres0(1), projection_parameteres0(3),
+  K0 << projection_parameters0(0), 0, projection_parameters0(2),
+      0, projection_parameters0(1), projection_parameters0(3),
       0, 0, 1;
-  K1 << projection_parameteres1(0), 0, projection_parameteres1(2),
-      0, projection_parameteres1(1), projection_parameteres1(3),
+  K1 << projection_parameters1(0), 0, projection_parameters1(2),
+      0, projection_parameters1(1), projection_parameters1(3),
       0, 0, 1;
 
   return (K0.inverse().transpose() * essentialMatrix(T_cam0_cam1) * K1.inverse());
+}
+
+inline void computeHorizontalStereoParameters(Size2i img_size,
+                                              Vector4& left_camera_parameters,
+                                              Vector4& left_distortion_coefficients,
+                                              Vector4& right_camera_parameters,
+                                              Vector4& right_distortion_coefficients,
+                                              Transformation T_L_R,
+                                              Matrix3 left_H,
+                                              Matrix3 right_H,
+                                              Vector4 transformed_left_camera_parameters,
+                                              Vector4 transformed_right_camera_parameters,
+                                              float& horizontal_offset)
+{
+  Quaternion avg_rotation = Quaternion::exp(-0.5*Quaternion::log(T_L_R.getRotation()));
 }
 
 } // namespace ze
---
+++ test/debug_main.cpp
@@ -0,0 +1,6 @@
+#include <ze/geometry/epipolar_geometry.hpp>
+
+int main(int argc, char** argv)
+{
+
+}
