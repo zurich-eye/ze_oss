729e13aa8d79357a9119adb95bd23c3985c66c3b
Matia Pizzoli <matia.pizzoli@gmail.com>
2016-08-13 20:10:32 +0200

    finished renaming left->cam0, right ->cam1

--- include/ze/geometry/epipolar_geometry.hpp
+++ include/ze/geometry/epipolar_geometry.hpp
@@ -156,36 +156,36 @@ inline std::pair<Rect, Rect> innerAndOuterRectangles(
 //!
 //! The function is specific for the horizontal-stereo case.
 //! \param img_size The size of the original (distorted) image.
-//! \param left_camera_parameters Vector of intrinsic parameters [fx, fy, cx, cy]
-//! for the left camera of the stereo pair.
-//! \param left_distortion_coefficients Vector of distortion coefficients
-//! for the left camera of the stereo pair.
-//! \param right_camera_parameters Vector of intrinsic parameters [fx, fy, cx, cy]
-//! for the right camera of the stereo pair.
-//! \param right_distortion_coefficients Vector of distortion coefficients
-//! for the left camera of the stereo pair.
-//! \param T_L_R Stereo extrinsic parameters, i.e. the transformation right-to-left.
-//! \return left_H rectifying homography for the left camera.
-//! \return right_H rectifying homography for the right camera.
-//! \return transformed_left_camera_parameters Output transformed parameters for the left camera.
-//! \return transformed_right_camera_parameters Output transformed parameters for the right camera.
+//! \param cam0_parameters Vector of intrinsic parameters [fx, fy, cx, cy]
+//! for the 0-th camera of the stereo pair.
+//! \param cam0_distortion_coefficients Vector of distortion coefficients
+//! for the 0-th camera of the stereo pair.
+//! \param cam1_parameters Vector of intrinsic parameters [fx, fy, cx, cy]
+//! for the 1st camera of the stereo pair.
+//! \param cam1_distortion_coefficients Vector of distortion coefficients
+//! for the 1st camera of the stereo pair.
+//! \param T_cam0_cam1 Stereo extrinsic parameters, i.e. the transformation right-to-left.
+//! \return cam0_H rectifying homography for the 0-th camera.
+//! \return cam1_H rectifying homography for the 1st camera.
+//! \return transformed_cam0_parameters Output transformed parameters for the 0-th camera.
+//! \return transformed_cam1_parameters Output transformed parameters for the 1st camera.
 //! \return horizontal_offset Output displacement for the rectified stereo pair.
 template<typename CameraModel,
          typename DistortionModel>
 inline std::tuple<Matrix3, Matrix3, Vector4, Vector4, FloatType>
 computeHorizontalStereoParameters(const Size2u& img_size,
-                                  const Vector4& left_camera_parameters,
-                                  const Vector4& left_distortion_coefficients,
-                                  const Vector4& right_camera_parameters,
-                                  const Vector4& right_distortion_coefficients,
-                                  const Transformation& T_L_R)
+                                  const Vector4& cam0_parameters,
+                                  const Vector4& cam0_distortion_coefficients,
+                                  const Vector4& cam1_parameters,
+                                  const Vector4& cam1_distortion_coefficients,
+                                  const Transformation& T_cam0_cam1)
 {
   //! Compute the recification homographies as in
   //! Trucco, Verry: Introductory techniques for 3D computer vision,
   //! Prentice Hall 1998, page 160.
   const Quaternion avg_rotation =
-      Quaternion::exp(-0.5*Quaternion::log(T_L_R.getRotation()));
-  const Vector3 transformed_t = avg_rotation.rotate(T_L_R.getPosition());
+      Quaternion::exp(-0.5*Quaternion::log(T_cam0_cam1.getRotation()));
+  const Vector3 transformed_t = avg_rotation.rotate(T_cam0_cam1.getPosition());
   const Vector3 e1 = transformed_t / transformed_t.norm();
   Vector3 e2(-transformed_t(1), transformed_t(0), 0);
   e2 = e2 / e2.norm();
@@ -196,19 +196,19 @@ computeHorizontalStereoParameters(const Size2u& img_size,
   rect_R.row(2) = e3.transpose();
 
   //! Rotate both cameras according to the average rotation.
-  const Matrix3 left_H = rect_R * avg_rotation.getRotationMatrix().transpose();
-  const Matrix3 right_H = rect_R * avg_rotation.getRotationMatrix();
+  const Matrix3 cam0_H = rect_R * avg_rotation.getRotationMatrix().transpose();
+  const Matrix3 cam1_H = rect_R * avg_rotation.getRotationMatrix();
 
   //! The images rectified according to left_H and right_H will not be contained
   //! in the same region of the image plane as the original image.
   //! Here we alter the focal lengths and the principal points to keep
   //! all points within the original image size.
-  const Vector4* const camera_parameter_ptrs[2] = {&left_camera_parameters,
-                                                   &right_camera_parameters};
-  const Vector4* const distortion_coefficient_ptrs[2] = {&left_distortion_coefficients,
-                                                         &right_distortion_coefficients};
-  const Matrix3* const homography_ptrs[2] = {&left_H,
-                                             &right_H};
+  const Vector4* const camera_parameter_ptrs[2] = {&cam0_parameters,
+                                                   &cam1_parameters};
+  const Vector4* const distortion_coefficient_ptrs[2] = {&cam0_distortion_coefficients,
+                                                         &cam1_distortion_coefficients};
+  const Matrix3* const homography_ptrs[2] = {&cam0_H,
+                                             &cam1_H};
 
   const FloatType nx = img_size[0];
   const FloatType ny = img_size[1];
@@ -257,71 +257,71 @@ computeHorizontalStereoParameters(const Size2u& img_size,
       transformed_principal_point.col(1) =
       transformed_principal_point.rowwise().mean();
 
-  Vector4 transformed_left_camera_parameters;
-  Vector4 transformed_right_camera_parameters;
-  transformed_left_camera_parameters << transformed_focal_length,
+  Vector4 transformed_cam0_parameters;
+  Vector4 transformed_cam1_parameters;
+  transformed_cam0_parameters << transformed_focal_length,
       transformed_focal_length,
       transformed_principal_point(0, 0),
       transformed_principal_point(1, 0);
-  transformed_right_camera_parameters << transformed_focal_length,
+  transformed_cam1_parameters << transformed_focal_length,
       transformed_focal_length,
       transformed_principal_point(0, 1),
       transformed_principal_point(1, 1);
 
-  std::pair<Rect, Rect> l_rects =
+  std::pair<Rect, Rect> cam0_rects =
       innerAndOuterRectangles<CameraModel, DistortionModel>(
-        img_size, left_camera_parameters,
-        transformed_left_camera_parameters,
-        left_distortion_coefficients,
-        left_H);
+        img_size, cam0_parameters,
+        transformed_cam0_parameters,
+        cam0_distortion_coefficients,
+        cam0_H);
 
-  std::pair<Rect, Rect> r_rects =
+  std::pair<Rect, Rect> cam1_rects =
       innerAndOuterRectangles<CameraModel, DistortionModel>(
-        img_size, right_camera_parameters,
-        transformed_right_camera_parameters,
-        right_distortion_coefficients,
-        right_H);
+        img_size, cam1_parameters,
+        transformed_cam1_parameters,
+        cam1_distortion_coefficients,
+        cam1_H);
 
   //! Determine s0, i.e. the scaling factor based on inner rectangles from both images.
   //! @todo (MPI) support different scales in [0, 1].
   //! Currently only scale = 0 is supported (s0).
-  //! Left image
+  //! Camera 0 image
   FloatType s0 = std::max(
-        transformed_principal_point(0, 0) / (transformed_principal_point(0, 0) - l_rects.first.x()),
-        transformed_principal_point(1, 0) / (transformed_principal_point(1, 0) - l_rects.first.y()));
+        transformed_principal_point(0, 0) / (transformed_principal_point(0, 0) - cam0_rects.first.x()),
+        transformed_principal_point(1, 0) / (transformed_principal_point(1, 0) - cam0_rects.first.y()));
 
   s0 = std::max(s0,
                 (nx - transformed_principal_point(0, 0)) /
-                (l_rects.first.x() + l_rects.first.width() - transformed_principal_point(0, 0)));
+                (cam0_rects.first.x() + cam0_rects.first.width() - transformed_principal_point(0, 0)));
 
   s0 = std::max(s0,
                 (ny - transformed_principal_point(1, 0)) /
-                (l_rects.first.y() + l_rects.first.height() - transformed_principal_point(1, 0)));
+                (cam0_rects.first.y() + cam0_rects.first.height() - transformed_principal_point(1, 0)));
 
-  //! Right image
+  //! Camera 1 image
   s0 = std::max(
-        transformed_principal_point(0, 1) / (transformed_principal_point(0, 1) - r_rects.first.x()),
-        transformed_principal_point(1, 1) / (transformed_principal_point(1, 1) - r_rects.first.y()));
+        transformed_principal_point(0, 1) / (transformed_principal_point(0, 1) - cam1_rects.first.x()),
+        transformed_principal_point(1, 1) / (transformed_principal_point(1, 1) - cam1_rects.first.y()));
 
   s0 = std::max(s0,
                 (nx - transformed_principal_point(0, 1)) /
-                (r_rects.first.x() + r_rects.first.width() - transformed_principal_point(0, 1)));
+                (cam1_rects.first.x() + cam1_rects.first.width() - transformed_principal_point(0, 1)));
 
   s0 = std::max(s0,
                 (ny - transformed_principal_point(1, 1)) /
-                (r_rects.first.y() + r_rects.first.height() - transformed_principal_point(1, 1)));
+                (cam1_rects.first.y() + cam1_rects.first.height() - transformed_principal_point(1, 1)));
 
-  transformed_left_camera_parameters(0) =
-      transformed_left_camera_parameters(1) =
-      transformed_right_camera_parameters(0) =
-      transformed_right_camera_parameters(1) = transformed_focal_length * s0;
+  transformed_cam0_parameters(0) =
+      transformed_cam0_parameters(1) =
+      transformed_cam1_parameters(0) =
+      transformed_cam1_parameters(1) = transformed_focal_length * s0;
 
   const FloatType horizontal_offset = transformed_t(0) * s0;
 
-  return std::make_tuple(left_H,
-                         right_H,
-                         transformed_left_camera_parameters,
-                         transformed_right_camera_parameters,
+  return std::make_tuple(cam0_H,
+                         cam1_H,
+                         transformed_cam0_parameters,
+                         transformed_cam1_parameters,
                          horizontal_offset);
 }
 
