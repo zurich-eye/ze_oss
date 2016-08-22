f7b488245c38ce0eba03060cc63316a8a407f986
Manuel Werlberger <code@werlberger.org>
2016-08-22 16:08:32 +0200

    gcc-5 complains about eigen maybe not having vars properly initialized

--- cmake/modules/ze_setup.cmake
+++ cmake/modules/ze_setup.cmake
@@ -1,7 +1,7 @@
 include(ze_options)
 
 # common flags
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Werror -fPIC -D_REENTRANT -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unknown-pragmas -Wno-error=deprecated-declarations")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Werror -fPIC -D_REENTRANT -Wno-maybe-uninitialized -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unknown-pragmas -Wno-error=deprecated-declarations")
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS} -O3 -fsee -fomit-frame-pointer -fno-signed-zeros -fno-math-errno -funroll-loops -ffast-math -fno-finite-math-only")
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g")
