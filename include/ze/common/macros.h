01184a6e6c51c9ac72569277812bd3d29abf439b
Christian Forster <ch.forster@gmail.com>
2016-08-22 12:44:25 -0700

    add macro

--- include/ze/common/macros.h
+++ include/ze/common/macros.h
@@ -7,6 +7,14 @@
   typedef std::shared_ptr<TypeName> Ptr;            \
   typedef std::shared_ptr<const TypeName> ConstPtr
 
-#define ZE_DELETE_COPY_ASSIGN(TypeName) \
-  TypeName(const TypeName&) = delete;         \
+#define ZE_DELETE_COPY_ASSIGN(TypeName)             \
+  TypeName(const TypeName&) = delete;               \
   void operator=(const TypeName&) = delete
+
+// Give the compiler a hint that an if statement is false.
+#define ZE_UNLIKELY(x)                              \
+  __builtin_expect((bool)(x), 0)
+
+// Give the compiler a hint that an if statement is true.
+#define ZE_LIKELY(x)                                \
+  __builtin_expect((bool)(x), 1)
