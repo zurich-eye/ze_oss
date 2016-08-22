5be27664bb0ca64ca23ec73748f819730dc6ad86
Christian Forster <forster@ifi.uzh.ch>
2016-08-22 17:21:21 +0200

    forgot file.

---
+++ include/ze/vi_simulation/camera_simulator_types.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <ze/common/types.h>
+
+namespace ze {
+
+struct CameraMeasurements
+{
+  //! Each column is a keypoint observation.
+  Keypoints keypoints_;
+
+  //! Global landmark index of each observed feature. The size of the vector is
+  //! the same as the number of columns in the keypoints block.
+  std::vector<int32_t> global_landmark_ids_;
+
+  //! Temporary track index of a landmark. If the landmark is
+  //! re-observed after a loop, it will be assigned a different id.
+  std::vector<int32_t> local_track_ids_;
+};
+using CameraMeasurementsVector = std::vector<CameraMeasurements>;
+
+} // namespace ze
