2ac2aa79d795ef9a12b6623d4cd599244163a844
Manuel Werlberger <code@werlberger.org>
2016-08-24 16:35:52 +0200

    changed license header

--- include/ze/geometry/vogiatzis_filter.h
+++ include/ze/geometry/vogiatzis_filter.h
@@ -1,4 +1,8 @@
-// FROM SVO
+// Copyright (C) ETH Zurich, Wyss Zurich, Zurich Eye - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+
+// from SVO
 
 #pragma once
 
--- test/test_triangulation.cpp
+++ test/test_triangulation.cpp
@@ -1,3 +1,7 @@
+// Copyright (C) ETH Zurich, Wyss Zurich, Zurich Eye - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+
 #include <random>
 #include <ze/common/test_entrypoint.h>
 #include <ze/common/matrix.h>
