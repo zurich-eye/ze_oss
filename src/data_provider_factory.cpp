5e21668705609a2f0432e4c52cb86bfeb0296f25
Manuel Werlberger <code@werlberger.org>
2016-08-24 16:35:52 +0200

    changed license header

--- src/data_provider_factory.cpp
+++ src/data_provider_factory.cpp
@@ -1,3 +1,7 @@
+// Copyright (C) ETH Zurich, Wyss Zurich, Zurich Eye - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+
 #include <ze/common/logging.hpp>
 #include <ze/data_provider/data_provider_factory.hpp>
 #include <ze/data_provider/data_provider_base.hpp>
