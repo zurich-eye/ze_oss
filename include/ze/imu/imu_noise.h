0b470c20d4b7ed6bfef25d6376cafe8337928201
Manuel Werlberger <code@werlberger.org>
2016-08-24 16:35:52 +0200

    changed license header

---
+++ include/ze/imu/imu_noise.h
@@ -0,0 +1,4 @@
+// Copyright (C) ETH Zurich, Wyss Zurich, Zurich Eye - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+
--- src/imu_noise_model.cpp
+++ src/imu_noise_model.cpp
@@ -1,3 +1,7 @@
+// Copyright (C) ETH Zurich, Wyss Zurich, Zurich Eye - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+
 #include "ze/imu/imu_noise_model.h"
 
 namespace ze {
