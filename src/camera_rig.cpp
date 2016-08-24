11228ac31ef5a0bca742703c44749d66f3d93deb
Manuel Werlberger <code@werlberger.org>
2016-08-24 16:35:52 +0200

    changed license header

--- src/camera_rig.cpp
+++ src/camera_rig.cpp
@@ -1,3 +1,7 @@
+// Copyright (C) ETH Zurich, Wyss Zurich, Zurich Eye - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+
 #include <ze/cameras/camera_rig.h>
 
 #include <imp/bridge/opencv/cv_bridge.hpp>
