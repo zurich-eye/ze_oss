6237b62ff49c8b5d419bc9f062123a83ac8a6623
Manuel Werlberger <code@werlberger.org>
2016-08-24 16:35:52 +0200

    changed license header

--- include/ze/common/thread_pool.hpp
+++ include/ze/common/thread_pool.hpp
@@ -1,27 +1,27 @@
-/* Adapted by ZE.
- *
- * Copyright (c) 2012 Jakob Progsch, Václav Zeman
- * From: https://github.com/progschj/ThreadPool
- *
- * This software is provided 'as-is', without any express or implied
- * warranty. In no event will the authors be held liable for any damages
- * arising from the use of this software.
- *
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
- *
- *    1. The origin of this software must not be misrepresented; you must not
- *    claim that you wrote the original software. If you use this software
- *    in a product, an acknowledgment in the product documentation would be
- *    appreciated but is not required.
- *
- *    2. Altered source versions must be plainly marked as such, and must not be
- *    misrepresented as being the original software.
- *
- *    3. This notice may not be removed or altered from any source
- *    distribution.
- */
+// Copyright (c) 2012 Jakob Progsch, Václav Zeman
+// From: https://github.com/progschj/ThreadPool
+// Copyright (C) 2016 ETH Zurich, Wyss Zurich, Zurich Eye
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+//    1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+//
+//    2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+//
+//    3. This notice may not be removed or altered from any source
+//    distribution.
+
+
 
 #pragma once
 
