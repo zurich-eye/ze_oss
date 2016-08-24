53ce658ac87857a0c6f5046d9c1ed8afe0fe7fa2
Manuel Werlberger <code@werlberger.org>
2016-08-24 16:35:52 +0200

    changed license header

--- src/viz_example_node.cpp
+++ src/viz_example_node.cpp
@@ -1,3 +1,7 @@
+// Copyright (C) ETH Zurich, Wyss Zurich, Zurich Eye - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+
 #include <ros/ros.h>
 #include <ze/common/logging.hpp>
 #include <gflags/gflags.h>
