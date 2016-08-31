df8c44c1b1608d2a01a31a0b43fe6bb073f66f94
Hannes Friederich <hannes.friederich@wysszurich.ch>
2016-08-31 13:50:37 +0200

    Replace safeGet() with a function that throws

--- include/ze/common/yaml_serialization.hpp
+++ include/ze/common/yaml_serialization.hpp
@@ -18,38 +18,21 @@
 
 namespace YAML {
 
-// A function to get a value from a YAML node with non-exception error handling.
-template<typename ValueType>
-bool safeGet(const YAML::Node& node, const std::string& key, ValueType* value)
+template <typename T>
+T extractChild(const YAML::Node& node, const std::string& key)
 {
-  CHECK_NOTNULL(value);
-  bool success = false;
-  if(!node.IsMap())
+  if (!node.IsMap())
   {
-    LOG(ERROR) << "Unable to get Node[\"" << key << "\"] because the node is not a map";
+    throw YAML::Exception(node.Mark(), "Node is not a map");
   }
-  else
+
+  const YAML::Node child = node[key];
+  if (!child)
   {
-    const YAML::Node sub_node = node[key];
-    if(sub_node)
-    {
-      try
-      {
-        *value = sub_node.as<ValueType>();
-        success = true;
-      }
-      catch(const YAML::Exception& e)
-      {
-        LOG(ERROR) << "Error getting key \"" << key << "\" as type "
-            << typeid(ValueType).name() << ": " << e.what();
-      }
-    }
-    else
-    {
-      LOG(ERROR) << "Key \"" << key << "\" does not exist";
-    }
+    throw YAML::Exception(node.Mark(), "key '" + key + "' does not exist");
   }
-  return success;
+
+  return child.as<T>();
 }
 
 // yaml serialization helper function for the Eigen3 Matrix object.
